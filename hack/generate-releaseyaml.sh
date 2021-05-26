eae269333a5a799a27fd86112ad1f8bc9f4474c7
Chmouel Boudjnah <chmouel@redhat.com>
2021-05-26 09:20:26 +0200

    Fix release yaml generation
    can't use [[ ]] with sh and anyway it was buggy to start with.
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- hack/generate-releaseyaml.sh
+++ hack/generate-releaseyaml.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env sh
-# Chmouel Boudjnah <chmouel@chmouel.com>
 set -euf
 
 export TARGET_REPO=${TARGET_REPO:-quay.io/openshift-pipeline/pipelines-as-code}
 export TARGET_BRANCH=${TARGET_BRANCH:-main}
 
 for file in $(find config -maxdepth 1 -name '*.yaml');do
-    [[ ${file} != "---"* ]] &&  echo "---"
+    head -1 ${file} | grep -q -- "---" || echo "---"
     sed -r "s,(.*image:.*)ko://github.com/openshift-pipelines/pipelines-as-code/cmd/.*,\1${TARGET_REPO}:${TARGET_BRANCH}\"," ${file}
 done
