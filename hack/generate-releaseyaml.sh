a6a9af2baddf283c8674e93d4f70c1607565320f
Chmouel Boudjnah <chmouel@redhat.com>
2021-06-03 16:33:16 +0200

    Allow redefining ns for multiple installs
    Add a ko mode too to Makefile, so we can redefine target namespace and as well
    ko resolve the images.
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- hack/generate-releaseyaml.sh
+++ hack/generate-releaseyaml.sh
@@ -1,10 +1,26 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -euf
 
 export TARGET_REPO=${TARGET_REPO:-quay.io/openshift-pipeline/pipelines-as-code}
 export TARGET_BRANCH=${TARGET_BRANCH:-main}
+export TARGET_NAMESPACE=${TARGET_NAMESPACE:-pipelines-as-code}
 
-for file in $(find config -maxdepth 1 -name '*.yaml'|sort -n);do
+MODE=${1:-""}
+
+if [[ -n ${MODE} && ${MODE} == ko ]];then
+    tmpfile=$(mktemp /tmp/.mm.XXXXXX)
+    clean() { rm -f ${tmpfile}; }
+    trap clean EXIT
+    ko resolve -f config/ > ${tmpfile}
+    files="${tmpfile}"
+else
+    files=$(find config -maxdepth 1 -name '*.yaml'|sort -n)
+fi
+
+for file in ${files};do
     head -1 ${file} | grep -q -- "---" || echo "---"
-    sed -r "s,(.*image:.*)ko://github.com/openshift-pipelines/pipelines-as-code/cmd/.*,\1${TARGET_REPO}:${TARGET_BRANCH}\"," ${file}
+    sed -r -e "s,(.*image:.*)ko://github.com/openshift-pipelines/pipelines-as-code/cmd/.*,\1${TARGET_REPO}:${TARGET_BRANCH}\"," \
+        -e "s/(namespace: )\w+.*/\1${TARGET_NAMESPACE}/g" \
+        -e "/kind: Namespace$/ { n;n;s/name: .*/name: ${TARGET_NAMESPACE}/;}" \
+        ${file}
 done
