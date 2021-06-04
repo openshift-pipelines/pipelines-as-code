50031a05cfc9c813656d81f75152160c0f321953
Chmouel Boudjnah <chmouel@redhat.com>
2021-06-04 09:47:01 +0200

    Fix generating release.yaml Namespace should be the first one
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- hack/dev/manual-push.sh
+++ hack/dev/manual-push.sh
@@ -2,6 +2,6 @@
 set -eux
 
 cd $(dirname $0)
-cd $(dirname $(pwd -P))
+cd $( dirname $(dirname $(pwd -P)))
 
-echo go run cmd/tknresolve/main.go  -f .tekton/push.yaml --generateName=true -p revision=main -p repo_url=https://github.com/openshift-pipelines/pipelines-as-code | kubectl create -f- -n pipelines-as-code-ci
+go run cmd/tknresolve/main.go  -f .tekton/push.yaml --generateName=true -p revision=main -p repo_url=https://github.com/openshift-pipelines/pipelines-as-code
