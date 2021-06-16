8e4487b82a920c263e587f8925039e7269ed6bad
Chmouel Boudjnah <chmouel@redhat.com>
2021-06-16 16:19:54 +0200

    Revert to 0.24.x
    If PAC is on 0.25 it would generate pipeline with the new :
    
     Spec *EmbeddedRunSpec `json:"spec,omitempty"`
    
    If the sever is not on 0.25 it would fail and since we support
    openshift-pipelines we don't want to fail
    
    So reverting.
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- vendor/github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1/pipelinerun_defaults.go
+++ vendor/github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1/pipelinerun_defaults.go
@@ -33,12 +33,8 @@ func (pr *PipelineRun) SetDefaults(ctx context.Context) {
 
 func (prs *PipelineRunSpec) SetDefaults(ctx context.Context) {
 	cfg := config.FromContextOrDefaults(ctx)
-	if prs.Timeout == nil && prs.Timeouts == nil {
+	if prs.Timeout == nil {
 		prs.Timeout = &metav1.Duration{Duration: time.Duration(cfg.Defaults.DefaultTimeoutMinutes) * time.Minute}
-	}
-
-	if prs.Timeouts != nil && prs.Timeouts.Pipeline == nil {
-		prs.Timeouts.Pipeline = &metav1.Duration{Duration: time.Duration(cfg.Defaults.DefaultTimeoutMinutes) * time.Minute}
 	}
 
 	defaultSA := cfg.Defaults.DefaultServiceAccount
