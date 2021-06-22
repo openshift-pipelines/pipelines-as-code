7106d412a90437b26837f2399212f2e381224fa4
Chmouel Boudjnah <chmouel@redhat.com>
2021-06-22 10:31:32 +0200

    Fix e2e to use v35 github
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- test/init_test.go
+++ test/init_test.go
@@ -11,7 +11,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/google/go-github/v34/github"
+	"github.com/google/go-github/v35/github"
 	"github.com/openshift-pipelines/pipelines-as-code/pkg/cli"
 	"github.com/openshift-pipelines/pipelines-as-code/pkg/webvcs"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
