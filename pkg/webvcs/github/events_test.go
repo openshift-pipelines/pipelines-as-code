4b012a662ddd2718d330e3a3fa422f170dee5838
Chmouel Boudjnah <chmouel@redhat.com>
2021-10-11 23:11:00 +0200

    rework parsepayload tests for github events

--- pkg/webvcs/github/events_test.go
+++ pkg/webvcs/github/events_test.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
 	"testing"
 
 	"github.com/google/go-github/v39/github"
@@ -36,6 +35,40 @@ Q1QWaigUQdpFfNCrqwJBANLgWaJV722PhQXOCmR+INvZ7ksIhJVcq/x1l2BYOLw2
 QsncVExbMiPa9Oclo5qLuTosS8qwHm1MJEytp3/SkB8=
 -----END RSA PRIVATE KEY-----`
 
+var sampleRepo = &github.Repository{
+	Owner: &github.User{
+		Login: github.String("owner"),
+	},
+	Name:          github.String("reponame"),
+	DefaultBranch: github.String("defaultbranch"),
+	HTMLURL:       github.String("https://github.com/owner/repo"),
+}
+
+var samplePRevent = github.PullRequestEvent{
+	PullRequest: &github.PullRequest{
+		Head: &github.PullRequestBranch{
+			SHA: github.String("sampleHeadsha"),
+			Ref: github.String("headred"),
+		},
+		Base: &github.PullRequestBranch{
+			SHA: github.String("basesha"),
+			Ref: github.String("baseref"),
+		},
+		User: &github.User{
+			Login: github.String("user"),
+		},
+	},
+	Repo: sampleRepo,
+}
+
+var samplePR = github.PullRequest{
+	Number: github.Int(54321),
+	Head: &github.PullRequestBranch{
+		SHA:  github.String("samplePRsha"),
+		Repo: sampleRepo,
+	},
+}
+
 // TODO: better testing matrix against only public function like we do for bitbucket-cloud
 func TestPayLoadFix(t *testing.T) {
 	b, err := ioutil.ReadFile("testdata/pull_request_with_newlines.json")
@@ -54,7 +87,7 @@ func TestPayLoadFix(t *testing.T) {
 		Client: fakeclient,
 	}
 
-	logger, _ := getLogger()
+	logger := getLogger()
 
 	event := &info.Event{
 		EventType:     "pull_request",
@@ -76,301 +109,202 @@ func TestPayLoadFix(t *testing.T) {
 	assert.NilError(t, err)
 }
 
-func TestParsePayloadRerequestFromPullRequest(t *testing.T) {
-	checkrunSender := "jean-pierre"
-	prOwner := "owner"
-	repoName := "repo"
-	prNumber := "123"
-	sha := "ParsePayloadRerequestFromPullRequestSHA"
-	checkrunEvent := fmt.Sprintf(`{"action": "rerequested",
-	"sender": {"login": "%s"},
-	"check_run": {"check_suite": {"pull_requests": [{"number": %s}]}},
-	"repository": {"name": "%s", "owner": {"login": "%s"}}}`,
-		checkrunSender, prNumber, repoName, prOwner)
+func TestParsePayLoad(t *testing.T) {
 	fakeclient, mux, _, teardown := ghtesthelper.SetupGH()
 	defer teardown()
-	mux.HandleFunc("/repos/"+prOwner+"/"+repoName+"/pulls/"+prNumber, func(rw http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprintf(rw, `{"head": {"sha": "%s", "ref": "123"}, "user": {"login": "%s"}}`, sha, prOwner)
-	})
-	mux.HandleFunc("/repos/owner/repo/commits/"+sha, func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprint(w, `{"commit": {"message": "HELLO"}}`)
-	})
-	mux.HandleFunc("/repos/owner/repo/git/commits/"+sha, func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprint(w, `{"commit": {"message": "HELLO"}}`)
-	})
-	ctx, _ := rtesting.SetupFakeContext(t)
-	gvcs := VCS{
-		Client: fakeclient,
-	}
-	logger, observer := getLogger()
-	event := &info.Event{
-		EventType:     "check_run",
-		TriggerTarget: "issue-recheck",
-	}
-	run := &params.Run{
-		Clients: clients.Clients{
-			Log: logger,
+
+	tests := []struct {
+		name               string
+		wantErrString      string
+		eventType          string
+		payloadEventStruct interface{}
+		jeez               string
+		triggerTarget      string
+		githubClient       *github.Client
+		muxReplies         map[string]interface{}
+		shaRet             string
+	}{
+		{
+			name:          "bad/unknow event",
+			wantErrString: "unknown X-Github-Event",
+			eventType:     "unknown",
 		},
-		Info: info.Info{
-			Event: event,
+		{
+			name:          "bad/invalid json",
+			wantErrString: "invalid character",
+			eventType:     "pull_request",
+			jeez:          "xxxx",
 		},
-	}
-	runinfo, err := gvcs.ParsePayload(ctx, run, checkrunEvent)
-	assert.NilError(t, err)
-
-	assert.Equal(t, prOwner, runinfo.Owner)
-	assert.Equal(t, repoName, runinfo.Repository)
-	assert.Assert(t, checkrunSender != runinfo.Sender)
-	assert.Equal(t, runinfo.EventType, "pull_request")
-	assert.Assert(t, strings.Contains(observer.TakeAll()[0].Message, "Recheck of PR"))
-}
-
-func TestParsePayloadRerequestFromPush(t *testing.T) {
-	sender := "jean-pierre"
-	headBranch := "tartonpion"
-	headSHA := "TestParsePayloadRerequestFromPushSHA"
-	owner := "owner"
-	repository := "repo"
-	url := fmt.Sprintf("https://github.com/%s/%s", owner, repository)
-	checkrunEvent := fmt.Sprintf(`{
-  "action": "rerequested",
-  "check_run": {
-    "check_suite": {
-      "head_branch": "%s",
-      "head_sha": "%s",
-      "pull_requests": []
-    }
-  },
-  "repository": {
-    "default_branch": "main",
-    "html_url": "%s",
-    "name": "%s",
-    "owner": {
-      "login": "%s"
-    }
-  },
-  "sender": {
-    "login": "%s"
-  }
-}`,
-		headBranch, headSHA, url, repository, owner, sender)
-	ctx, _ := rtesting.SetupFakeContext(t)
-	fakeclient, mux, _, teardown := ghtesthelper.SetupGH()
-	defer teardown()
-	gvcs := VCS{
-		Client: fakeclient,
-	}
-	mux.HandleFunc("/repos/owner/repo/commits/"+headSHA, func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprint(w, `{"commit": {"message": "HELLO"}}`)
-	})
-	mux.HandleFunc("/repos/owner/repo/git/commits/"+headSHA, func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprint(w, `{"commit": {"message": "HELLO"}}`)
-	})
-
-	event := &info.Event{
-		EventType:     "check_run",
-		TriggerTarget: "issue-recheck",
-	}
-	logger, _ := getLogger()
-
-	run := &params.Run{
-		Clients: clients.Clients{
-			Log: logger,
+		{
+			name:               "bad/not supported",
+			wantErrString:      "this event is not supported",
+			eventType:          "pull_request_review_comment",
+			payloadEventStruct: github.PullRequestReviewCommentEvent{Action: github.String("created")},
 		},
-		Info: info.Info{
-			Event: event,
+		{
+			name:               "bad/check run only issue recheck supported",
+			wantErrString:      "only issue recheck is supported",
+			eventType:          "check_run",
+			triggerTarget:      "nonopetitrobot",
+			payloadEventStruct: github.CheckRunEvent{Action: github.String("created")},
+			githubClient:       fakeclient,
 		},
-	}
-	runinfo, err := gvcs.ParsePayload(ctx, run, checkrunEvent)
-	assert.NilError(t, err)
-
-	assert.Equal(t, runinfo.EventType, "push")
-	assert.Equal(t, runinfo.HeadBranch, headBranch)
-	assert.Equal(t, runinfo.Owner, owner)
-	assert.Equal(t, runinfo.Repository, repository)
-	assert.Equal(t, runinfo.URL, url)
-	assert.Assert(t, sender == runinfo.Sender) // TODO: should it be set to the push sender?
-}
-
-func TestParsePayLoadRetest(t *testing.T) {
-	issueSender := "tartanpion"
-	prOwner := "user1"
-	repoOwner := "openshift"
-	repoName := "pipelines"
-	prNumber := "123"
-	sha := "TestParsePayLoadRetestSHA"
-
-	fakeclient, mux, _, teardown := ghtesthelper.SetupGH()
-	defer teardown()
-	mux.HandleFunc("/repos/"+prOwner+"/"+repoName+"/pulls/"+prNumber, func(rw http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprintf(rw, `{"head": {"sha": "%s", "ref": "123"}, "user": {"login": "%s"}}`, sha, prOwner)
-	})
-	mux.HandleFunc("/repos/user1/pipelines/commits/"+sha, func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprint(w, `{"commit": {"message": "HELLO"}}`)
-	})
-	mux.HandleFunc("/repos/user1/pipelines/git/commits/"+sha, func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprint(w, `{"commit": {"message": "HELLO"}}`)
-	})
-	issueEvent := fmt.Sprintf(`{
-  "sender": {
-	"login": "%s"
-  },
-  "repository": {
-	"name": "%s",
-	"owner": {
-	  "login": "%s"
-	}
-  },
-  "issue": {
-	"pull_request": {
-	  "html_url": "https://github.com/%s/%s/pull/%s"
-	}
-  }
-}`, issueSender, repoName, prOwner, repoName, repoOwner, prNumber)
-
-	ctx, _ := rtesting.SetupFakeContext(t)
-	logger, observer := getLogger()
-	gvcs := VCS{
-		Client: fakeclient,
-	}
-	// TODO
-	event := &info.Event{
-		EventType:     "issue_comment",
-		TriggerTarget: "issue_comment",
-	}
-	run := &params.Run{
-		Clients: clients.Clients{
-			Log: logger,
+		{
+			name:               "bad/check run only with github apps",
+			wantErrString:      "only supported with github apps",
+			eventType:          "check_run",
+			payloadEventStruct: github.CheckRunEvent{Action: github.String("created")},
 		},
-		Info: info.Info{
-			Event: event,
-			Pac: &info.PacOpts{
-				VCSToken: "TOKENSET",
+		{
+			name:               "bad/issue comment retest only with github apps",
+			wantErrString:      "only supported with github apps",
+			eventType:          "issue_comment",
+			payloadEventStruct: github.IssueCommentEvent{Action: github.String("created")},
+		},
+		{
+			name:               "bad/issue comment not coming from pull request",
+			eventType:          "issue_comment",
+			githubClient:       fakeclient,
+			payloadEventStruct: github.IssueCommentEvent{Issue: &github.Issue{}},
+			wantErrString:      "issue comment is not coming from a pull_request",
+		},
+		{
+			name:         "bad/issue comment invalid pullrequest",
+			eventType:    "issue_comment",
+			githubClient: fakeclient,
+			payloadEventStruct: github.IssueCommentEvent{Issue: &github.Issue{
+				PullRequestLinks: &github.PullRequestLinks{
+					HTMLURL: github.String("/bad"),
+				},
+			}},
+			wantErrString: "bad pull request number",
+		},
+		{
+			name:          "bad/rerequest error fetching PR",
+			githubClient:  fakeclient,
+			eventType:     "check_run",
+			triggerTarget: "issue-recheck",
+			wantErrString: "404",
+			payloadEventStruct: github.CheckRunEvent{
+				Repo: sampleRepo,
+				CheckRun: &github.CheckRun{
+					CheckSuite: &github.CheckSuite{
+						PullRequests: []*github.PullRequest{&samplePR},
+					},
+				},
 			},
+			shaRet: "samplePRsha",
 		},
-	}
 
-	runinfo, err := gvcs.ParsePayload(ctx, run, issueEvent)
-	assert.NilError(t, err)
-	assert.Equal(t, prOwner, runinfo.Owner)
-	// Make sure the PR owner is the runinfo.Owner and not the issueSender
-	assert.Assert(t, issueSender != runinfo.Owner)
-	firstObservedMessage := observer.TakeAll()[0].Message
-	assert.Assert(t, strings.Contains(firstObservedMessage, "recheck"))
-	assert.Equal(t, runinfo.EventType, "pull_request")
+		{
+			name:          "good/rerequest on pull request",
+			eventType:     "check_run",
+			githubClient:  fakeclient,
+			triggerTarget: "issue-recheck",
+			payloadEventStruct: github.CheckRunEvent{
+				Repo: sampleRepo,
+				CheckRun: &github.CheckRun{
+					CheckSuite: &github.CheckSuite{
+						PullRequests: []*github.PullRequest{&samplePR},
+					},
+				},
+			},
+			muxReplies: map[string]interface{}{"/repos/owner/reponame/pulls/54321": samplePR},
+			shaRet:     "samplePRsha",
+		},
+		{
+			name:          "good/rerequest on push",
+			eventType:     "check_run",
+			githubClient:  fakeclient,
+			triggerTarget: "issue-recheck",
+			payloadEventStruct: github.CheckRunEvent{
+				Repo: sampleRepo,
+				CheckRun: &github.CheckRun{
+					CheckSuite: &github.CheckSuite{
+						HeadSHA: github.String("headSHACheckSuite"),
+					},
+				},
+			},
+			shaRet: "headSHACheckSuite",
+		},
+		{
+			name:         "good/issue comment",
+			eventType:    "issue_comment",
+			githubClient: fakeclient,
+			payloadEventStruct: github.IssueCommentEvent{
+				Issue: &github.Issue{
+					PullRequestLinks: &github.PullRequestLinks{
+						HTMLURL: github.String("/666"),
+					},
+				},
+				Repo: sampleRepo,
+			},
+			muxReplies: map[string]interface{}{"/repos/owner/reponame/pulls/666": samplePR},
+			shaRet:     "samplePRsha",
+		},
+		{
+			name:               "good/pull request",
+			eventType:          "pull_request",
+			triggerTarget:      "pull_request",
+			payloadEventStruct: samplePRevent,
+			shaRet:             "sampleHeadsha",
+		},
+		{
+			name:          "good/push",
+			eventType:     "push",
+			triggerTarget: "push",
+			payloadEventStruct: github.PushEvent{
+				Repo: &github.PushEventRepository{
+					Owner: &github.User{Login: github.String("owner")},
+					Name:  github.String("pushRepo"),
+				},
+				HeadCommit: &github.HeadCommit{ID: github.String("SHAPush")},
+			},
+			shaRet: "SHAPush",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx, _ := rtesting.SetupFakeContext(t)
 
-	// We cannot parse payload without a token when getting from issue_comment
-	gvcs.Client = nil
-	runinfo, err = gvcs.ParsePayload(ctx, run, issueEvent)
-	assert.ErrorContains(t, err, "gitops style comments operation is only supported with github apps")
-	assert.Assert(t, runinfo.Event == nil)
-}
+			for key, value := range tt.muxReplies {
+				mux.HandleFunc(key, func(rw http.ResponseWriter, r *http.Request) {
+					bjeez, _ := json.Marshal(value)
+					fmt.Fprint(rw, string(bjeez))
+				})
+			}
+			gvcs := VCS{
+				Client: tt.githubClient,
+			}
+			logger := getLogger()
+			event := &info.Event{
+				EventType:     tt.eventType,
+				TriggerTarget: tt.triggerTarget,
+			}
 
-func TestParsePayload(t *testing.T) {
-	b, err := ioutil.ReadFile("testdata/pull_request.json")
-	assert.NilError(t, err)
-	fakeclient, mux, _, teardown := ghtesthelper.SetupGH()
-	defer teardown()
-	mux.HandleFunc("/repos/chmouel/scratchpad/commits/cc8334de8e056317d18bd00c2588c3f7c95af294", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprint(w, `{"commit": {"message": "HELLO"}}`)
-	})
-	mux.HandleFunc("/repos/chmouel/scratchpad/git/commits/cc8334de8e056317d18bd00c2588c3f7c95af294", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprint(w, `{"commit": {"message": "HELLO"}}`)
-	})
-	gvcs := VCS{
-		Client: fakeclient,
+			run := &params.Run{
+				Clients: clients.Clients{
+					Log: logger,
+				},
+				Info: info.Info{
+					Event: event,
+				},
+			}
+			bjeez, _ := json.Marshal(tt.payloadEventStruct)
+			jeez := string(bjeez)
+			if tt.jeez != "" {
+				jeez = tt.jeez
+			}
+			ret, err := gvcs.ParsePayload(ctx, run, jeez)
+			if tt.wantErrString != "" {
+				assert.ErrorContains(t, err, tt.wantErrString)
+				return
+			}
+			assert.NilError(t, err)
+			assert.Assert(t, ret != nil)
+			assert.Equal(t, tt.shaRet, ret.SHA)
+		})
 	}
-	ctx, _ := rtesting.SetupFakeContext(t)
-	logger, _ := getLogger()
-
-	event := &info.Event{
-		EventType:     "pull_request",
-		TriggerTarget: "pull_request",
-	}
-	run := &params.Run{
-		Clients: clients.Clients{
-			Log: logger,
-		},
-		Info: info.Info{
-			Event: event,
-		},
-	}
-	runinfo, err := gvcs.ParsePayload(ctx, run, string(b))
-	assert.NilError(t, err)
-	assert.Assert(t, runinfo.BaseBranch == "master")
-	assert.Assert(t, runinfo.Owner == "chmouel")
-	assert.Assert(t, runinfo.Repository == "scratchpad")
-	assert.Equal(t, runinfo.EventType, "pull_request")
-	assert.Assert(t, runinfo.URL == "https://github.com/chmouel/scratchpad")
-}
-
-func TestParsePayloadInvalid(t *testing.T) {
-	ctx, _ := rtesting.SetupFakeContext(t)
-	gvcs := VCS{
-		Token:  github.String("none"),
-		APIURL: github.String("nothing"),
-	}
-	logger, _ := getLogger()
-	event := &info.Event{
-		EventType:     "pull_request",
-		TriggerTarget: "pull_request",
-	}
-	run := &params.Run{
-		Clients: clients.Clients{
-			Log: logger,
-		},
-		Info: info.Info{
-			Event: event,
-		},
-	}
-	_, err := gvcs.ParsePayload(ctx, run, "hello moto")
-	assert.ErrorContains(t, err, "invalid character")
-}
-
-func TestParsePayloadUnkownEvent(t *testing.T) {
-	ctx, _ := rtesting.SetupFakeContext(t)
-	gvcs := VCS{
-		Token:  github.String("none"),
-		APIURL: github.String(""),
-	}
-	logger, _ := getLogger()
-	event := &info.Event{
-		EventType:     "foo",
-		TriggerTarget: "foo",
-	}
-	run := &params.Run{
-		Clients: clients.Clients{
-			Log: logger,
-		},
-		Info: info.Info{
-			Event: event,
-		},
-	}
-	_, err := gvcs.ParsePayload(ctx, run, "{\"hello\": \"moto\"}")
-	assert.ErrorContains(t, err, "unknown X-Github-Event")
-}
-
-func TestParsePayCannotParse(t *testing.T) {
-	ctx, _ := rtesting.SetupFakeContext(t)
-	gvcs := VCS{
-		Token:  github.String("none"),
-		APIURL: github.String(""),
-	}
-	logger, _ := getLogger()
-	event := &info.Event{
-		EventType:     "gollum",
-		TriggerTarget: "gollum",
-	}
-	run := &params.Run{
-		Clients: clients.Clients{
-			Log: logger,
-		},
-		Info: info.Info{
-			Event: event,
-		},
-	}
-	_, err := gvcs.ParsePayload(ctx, run, "{}")
-	assert.Error(t, err, "this event is not supported")
 }
 
 func TestAppTokenGeneration(t *testing.T) {
@@ -500,33 +434,10 @@ func TestAppTokenGeneration(t *testing.T) {
 			envRemove := env.PatchAll(t, tt.envs)
 			defer envRemove()
 
-			payloadEvent := github.PullRequestEvent{
-				PullRequest: &github.PullRequest{
-					Head: &github.PullRequestBranch{
-						SHA: github.String("headsha"),
-						Ref: github.String("headred"),
-					},
-					Base: &github.PullRequestBranch{
-						SHA: github.String("basesha"),
-						Ref: github.String("baseref"),
-					},
-					User: &github.User{
-						Login: github.String("user"),
-					},
-				},
-				Repo: &github.Repository{
-					Owner: &github.User{
-						Login: github.String("owner"),
-					},
-					Name:          github.String("RepoName"),
-					DefaultBranch: github.String("DefaultBranch"),
-					HTMLURL:       github.String("https://github.com/owner/repo"),
-				},
-			}
 			ctx, _ := rtesting.SetupFakeContext(t)
-			jeez, _ := json.Marshal(payloadEvent)
+			jeez, _ := json.Marshal(samplePRevent)
 			gvcs := VCS{}
-			logger, _ := getLogger()
+			logger := getLogger()
 			event := &info.Event{
 				EventType:     "pull_request",
 				TriggerTarget: "pull_request",
