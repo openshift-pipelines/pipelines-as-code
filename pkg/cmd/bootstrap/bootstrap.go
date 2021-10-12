fc0320211e36f0e7063e3decaa097d9ab2dac85f
Chmouel Boudjnah <chmouel@redhat.com>
2021-10-12 10:50:01 +0200

    Allow skipping app creation or install
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- pkg/cmd/bootstrap/bootstrap.go
+++ pkg/cmd/bootstrap/bootstrap.go
@@ -26,6 +26,8 @@ var vcsTargets = []string{"github-app", "github-enteprise-app"}
 type bootstrapOpts struct {
 	vcsType         string
 	installNightly  bool
+	skipInstall     bool
+	skipGithubAPP   bool
 	webserverPort   int
 	cliOpts         *params.PacCliOpts
 	ioStreams       *ui.IOStreams
@@ -53,6 +55,44 @@ const successTmpl = `
 <html><body>You have <span style=\"color: green\">successfully</span> created a new Github application, go back to the tkn pac cli to finish the installation.</body></html>
 `
 
+func install(ctx context.Context, run *params.Run, opts *bootstrapOpts) error {
+	// nolint:forbidigo
+	fmt.Println("🏃 Checking if Pipelines as Code is installed.")
+	if checkNS(ctx, run, opts) {
+		// nolint:forbidigo
+		fmt.Println("👌 Pipelines as Code is installed.")
+	} else if err := installPac(ctx, opts); err != nil {
+		return err
+	}
+	return nil
+}
+
+func createSecret(ctx context.Context, run *params.Run, opts *bootstrapOpts) error {
+	var err error
+	opts.recreateSecret = checkSecret(ctx, run, opts)
+	opts.RouteName, err = detectOpenShiftRoute(ctx, run, opts)
+	if err != nil {
+		return err
+	}
+
+	if err := askQuestions(opts); err != nil {
+		return err
+	}
+
+	if opts.recreateSecret {
+		if err := deleteSecret(ctx, run, opts); err != nil {
+			return err
+		}
+	}
+
+	jeez, err := generateManifest(opts)
+	if err != nil {
+		return err
+	}
+
+	return startWebServer(ctx, opts, run, string(jeez))
+}
+
 func Command(run *params.Run, ioStreams *ui.IOStreams) *cobra.Command {
 	opts := &bootstrapOpts{}
 	cmd := &cobra.Command{
@@ -69,44 +109,25 @@ func Command(run *params.Run, ioStreams *ui.IOStreams) *cobra.Command {
 			}
 			opts.ioStreams.SetColorEnabled(!opts.cliOpts.NoColoring)
 
-			err = run.Clients.NewClients(&run.Info)
-			if err != nil {
+			if err := run.Clients.NewClients(&run.Info); err != nil {
 				return err
 			}
 
-			// nolint:forbidigo
-			fmt.Println("🏃 Checking if Pipelines as Code is installed.")
-			if checkNS(ctx, run, opts) {
-				// nolint:forbidigo
-				fmt.Println("👌 Pipelines as Code is installed.")
-			} else if err := installPac(ctx, opts); err != nil {
-				return err
-			}
-
-			opts.recreateSecret = checkSecret(ctx, run, opts)
-			opts.RouteName, err = detectOpenShiftRoute(ctx, run, opts)
-			if err != nil {
-				return err
-			}
-
-			if err := askQuestions(opts); err != nil {
-				return err
-			}
-
-			if opts.recreateSecret {
-				if err := deleteSecret(ctx, run, opts); err != nil {
+			if !opts.skipInstall {
+				if err := install(ctx, run, opts); err != nil {
 					return err
 				}
 			}
 
-			jeez, err := generateManifest(opts)
-			if err != nil {
-				return err
+			if !opts.skipGithubAPP {
+				if err := createSecret(ctx, run, opts); err != nil {
+					return err
+				}
 			}
-
-			return startWebServer(ctx, opts, run, string(jeez))
+			return nil
 		},
 	}
+
 	cmd.PersistentFlags().StringVar(&opts.GithubApplicationName, "github-application-name", "", "Github Application Name")
 	cmd.PersistentFlags().StringVar(&opts.GithubApplicationURL, "github-application-url", "", "Github Application URL")
 	cmd.PersistentFlags().StringVar(&opts.RouteName, "route-url", "", "the URL for the eventlistenner")
@@ -114,6 +135,9 @@ func Command(run *params.Run, ioStreams *ui.IOStreams) *cobra.Command {
 	cmd.PersistentFlags().BoolVar(&opts.installNightly, "nightly", false, "Wether to install the nightly Pipelines as Code")
 	cmd.PersistentFlags().IntVar(&opts.webserverPort, "webserver-port", 8080, "webserver-port")
 	cmd.PersistentFlags().StringVarP(&opts.targetNamespace, "namespace", "n", pacNS, "target namespace where pac is installed")
+
+	cmd.PersistentFlags().BoolVar(&opts.skipInstall, "skip-install", false, "skip Pipelines as Code installation")
+	cmd.PersistentFlags().BoolVar(&opts.skipGithubAPP, "skip-github-app", false, "skip creating github application")
 
 	cmd.PersistentFlags().StringVarP(&opts.vcsType, "install-type", "t", defaultVCSType,
 		fmt.Sprintf("target install type, choices are: %s ", strings.Join(vcsTargets, ", ")))
