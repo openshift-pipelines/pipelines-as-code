a8c522b9574b254bba2a6bd5747ffdb26c974643
Chmouel Boudjnah <chmouel@redhat.com>
2021-11-18 13:13:59 +0100

    Add make-release script
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

---
+++ hack/make-release.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+VERSION=${1:-""}
+remote=git@github.com:openshift-pipelines/pipelines-as-code
+
+[[ -z ${VERSION} ]] && {
+    echo "need a version"
+    exit 1
+}
+
+sed -i "s,\(https://github.com/openshift-pipelines/pipelines-as-code/blob/\)[^/]*,\1${VERSION}," README.md
+sed -i "s/^VERSION=.*/VERSION=${VERSION}/" docs/install.md
+
+git status -uno|grep -q "nothing to commit" || {
+    echo "there is change locally, commit them first"
+    git status -uno
+    exit 1
+}
+
+git switch main
+
+git commit -S -m "Update documentation for Release ${VERSION}" docs/install.md README.md
+git tag -s ${VERSION} -m "Release ${VERSION}"
+git push ${remote} refs/heads/main
+git push --tags ${remote} refs/tags/${VERSION}
