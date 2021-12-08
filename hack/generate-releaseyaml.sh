340ec5bdd6fefd15b23ed71730759b979600c8b8
Chmouel Boudjnah <chmouel@redhat.com>
2021-12-08 16:16:29 +0100

    Add release version label to all resources
    Add the released version label to all files in config
    
    Closes #381
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- hack/generate-releaseyaml.sh
+++ hack/generate-releaseyaml.sh
@@ -5,6 +5,7 @@ export TARGET_REPO=${TARGET_REPO:-quay.io/openshift-pipeline/pipelines-as-code}
 export TARGET_BRANCH=${TARGET_BRANCH:-main}
 export TARGET_NAMESPACE=${TARGET_NAMESPACE:-pipelines-as-code}
 export TARGET_OPENSHIFT=${TARGET_OPENSHIFT:-""}
+export TARGET_PAC_VERSION=${PAC_VERSION:-"devel"}
 
 MODE=${1:-""}
 
@@ -32,6 +33,7 @@ for file in ${files};do
     head -1 ${file} | grep -q -- "---" || echo "---"
     sed -r -e "s,(.*image:.*)ko://github.com/openshift-pipelines/pipelines-as-code/cmd/.*,\1${TARGET_REPO}:${TARGET_BRANCH}\"," \
         -e "s/(namespace: )\w+.*/\1${TARGET_NAMESPACE}/g" \
+        -e "s,app.kubernetes.io/version:.*,app.kubernetes.io/version: \"${TARGET_PAC_VERSION}\"," \
         -e "/kind: Namespace$/ { n;n;s/name: .*/name: ${TARGET_NAMESPACE}/;}" \
         ${file}
 done
