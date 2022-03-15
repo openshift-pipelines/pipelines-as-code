665ca3f4d770cf3daae1b1c0091d27ad1ef5dcbf
Chmouel Boudjnah <chmouel@redhat.com>
2022-03-15 14:39:57 +0100

    Improve replay-last-pr script
    * preview the pipelinerun to select
    * generate a shell script easy to rerun.
    * add more comments
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

---
+++ pkg/matcher/cel.go
@@ -0,0 +1,41 @@
+package matcher
+
+import (
+	"fmt"
+
+	"github.com/google/cel-go/cel"
+	"github.com/google/cel-go/checker/decls"
+	"github.com/google/cel-go/common/types/ref"
+)
+
+func celEvaluate(expr string, data map[string]interface{}) (ref.Val, error) {
+	env, err := cel.NewEnv(
+		cel.Declarations(
+			decls.NewVar("event", decls.String),
+			decls.NewVar("target_branch", decls.String),
+			decls.NewVar("source_branch", decls.String)))
+	if err != nil {
+		return nil, err
+	}
+
+	parsed, issues := env.Parse(expr)
+	if issues != nil && issues.Err() != nil {
+		return nil, fmt.Errorf("failed to parse expression %#v: %w", expr, issues.Err())
+	}
+
+	checked, issues := env.Check(parsed)
+	if issues != nil && issues.Err() != nil {
+		return nil, fmt.Errorf("expression %#v check failed: %w", expr, issues.Err())
+	}
+
+	prg, err := env.Program(checked)
+	if err != nil {
+		return nil, fmt.Errorf("expression %#v failed to create a Program: %w", expr, err)
+	}
+
+	out, _, err := prg.Eval(data)
+	if err != nil {
+		return nil, fmt.Errorf("expression %#v failed to evaluate: %w", expr, err)
+	}
+	return out, nil
+}
