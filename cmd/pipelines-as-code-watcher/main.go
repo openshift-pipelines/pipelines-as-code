bb7f8f5cac3f21b1f7678bb994b8010e0f668a53
Shivam Mukhade <smukhade@redhat.com>
2022-05-23 13:31:31 +0530

    Add probes for pac watcher
    Signed-off-by: Shivam Mukhade <smukhade@redhat.com>

--- cmd/pipelines-as-code-watcher/main.go
+++ cmd/pipelines-as-code-watcher/main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/openshift-pipelines/pipelines-as-code/pkg/reconciler"
 	"knative.dev/pkg/injection/sharedmain"
 )
 
+const probesPort = "8080"
+
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/live", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		_, _ = fmt.Fprint(w, "ok")
+	})
+	go func() {
+		// start the web server on port and accept requests
+		log.Printf("Readiness and health check server listening on port %s", probesPort)
+		log.Fatal(http.ListenAndServe(":"+probesPort, mux))
+	}()
+
 	sharedmain.Main("pac-watcher", reconciler.NewController())
 }
