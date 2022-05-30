b662fa446e1791c24b26622d6f51021574dd653f
Shivam Mukhade <smukhade@redhat.com>
2022-05-30 15:58:50 +0530

    Adds webhook to validate repository url
    this add validation webhook for repository so that
    we dont allow user to create multiple respositories with
    same git url.
    
    Signed-off-by: Shivam Mukhade <smukhade@redhat.com>

---
+++ cmd/pipelines-as-code-webhook/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	validation_webhook "github.com/openshift-pipelines/pipelines-as-code/pkg/webhook"
+	"knative.dev/pkg/configmap"
+	"knative.dev/pkg/controller"
+	"knative.dev/pkg/injection"
+	"knative.dev/pkg/injection/sharedmain"
+	"knative.dev/pkg/signals"
+	"knative.dev/pkg/webhook"
+	"knative.dev/pkg/webhook/certificates"
+)
+
+func main() {
+	ctx := signals.NewContext()
+
+	serviceName := os.Getenv("WEBHOOK_SERVICE_NAME")
+	if serviceName == "" {
+		serviceName = "pipelines-as-code-webhook"
+	}
+	secretName := os.Getenv("WEBHOOK_SECRET_NAME")
+	if secretName == "" {
+		secretName = "pipelines-as-code-webhook-certs"
+	}
+	// Set up a signal context with our webhook options
+	ctx = webhook.WithOptions(ctx, webhook.Options{
+		ServiceName: serviceName,
+		Port:        8443,
+		SecretName:  secretName,
+	})
+
+	sharedmain.WebhookMainWithConfig(ctx, "pipelines-as-code-webhook",
+		injection.ParseAndGetRESTConfigOrDie(),
+		certificates.NewController,
+		newValidationAdmissionController,
+	)
+}
+
+func newValidationAdmissionController(ctx context.Context, _ configmap.Watcher) *controller.Impl {
+	return validation_webhook.NewAdmissionController(ctx,
+
+		// Name of the resource webhook.
+		"validation.pipelinesascode.tekton.dev",
+
+		// The path on which to serve the webhook.
+		"/validate",
+
+		// A function that infuses the context passed to Validate/SetDefaults with custom metadata.
+		func(ctx context.Context) context.Context {
+			return ctx
+		},
+
+		// Whether to disallow unknown fields.
+		true,
+	)
+}
---
+++ pkg/webhook/validation.go
@@ -0,0 +1,56 @@
+package webhook
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/openshift-pipelines/pipelines-as-code/pkg/apis/pipelinesascode/v1alpha1"
+	"github.com/openshift-pipelines/pipelines-as-code/pkg/generated/clientset/versioned"
+	v1 "k8s.io/api/admission/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"knative.dev/pkg/webhook"
+)
+
+var universalDeserializer = serializer.NewCodecFactory(runtime.NewScheme()).UniversalDeserializer()
+
+// Path implements AdmissionController
+func (ac *reconciler) Path() string {
+	return ac.path
+}
+
+// Admit implements AdmissionController
+func (ac *reconciler) Admit(ctx context.Context, request *v1.AdmissionRequest) *v1.AdmissionResponse {
+	raw := request.Object.Raw
+	repo := v1alpha1.Repository{}
+	if _, _, err := universalDeserializer.Decode(raw, nil, &repo); err != nil {
+		return webhook.MakeErrorStatus("validation failed: %v", err)
+	}
+
+	exist, err := checkIfRepoExist(ctx, ac.pacClient, &repo, "")
+	if err != nil {
+		return webhook.MakeErrorStatus("validation failed: %v", err)
+	}
+
+	if exist {
+		return webhook.MakeErrorStatus(fmt.Sprintf("repository already exist with url: %s", repo.Spec.URL))
+	}
+
+	return &v1.AdmissionResponse{Allowed: true}
+}
+
+func checkIfRepoExist(ctx context.Context, pac versioned.Interface, repo *v1alpha1.Repository, ns string) (bool, error) {
+	repositories, err := pac.PipelinesascodeV1alpha1().Repositories(ns).List(
+		ctx, metav1.ListOptions{})
+	if err != nil {
+		return false, err
+	}
+	for i := len(repositories.Items) - 1; i >= 0; i-- {
+		repoFromCluster := repositories.Items[i]
+		if repoFromCluster.Spec.URL == repo.Spec.URL {
+			return true, nil
+		}
+	}
+	return false, nil
+}
