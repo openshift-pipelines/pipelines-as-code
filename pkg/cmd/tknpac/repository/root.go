92e029b19e04cb314a247efee052d7c3780a2837
Shivam Mukhade <smukhade@redhat.com>
2022-06-01 12:15:57 +0530

    cleanup repo describe command
    Signed-off-by: Shivam Mukhade <smukhade@redhat.com>

--- pkg/cmd/tknpac/repository/root.go
+++ pkg/cmd/tknpac/repository/root.go
@@ -17,7 +17,6 @@ func Root(clients *params.Run, ioStreams *cli.IOStreams) *cobra.Command {
 			"commandType": "main",
 		},
 	}
-	cmd.AddCommand(DescribeCommand(clients, ioStreams))
 	cmd.AddCommand(DeleteCommand(clients, ioStreams))
 
 	return cmd
