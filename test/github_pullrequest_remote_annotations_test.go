608517d183fbdb77b2741431ac0c791c3034b0bd
Chmouel Boudjnah <chmouel@redhat.com>
2022-07-26 16:59:36 +0200

    make sure we use v1beta1
    since now deprecated in pipeline 0.38.0 and fail in a weird ssl error
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- test/github_pullrequest_remote_annotations_test.go
+++ test/github_pullrequest_remote_annotations_test.go
@@ -99,7 +99,7 @@ func TestGithubPullRequestRemoteAnnotations(t *testing.T) {
 	assert.Equal(t, title, *laststatus.Title)
 	assert.Assert(t, *laststatus.LogURL != "")
 
-	pr, err := runcnx.Clients.Tekton.TektonV1alpha1().PipelineRuns(targetNS).Get(ctx, laststatus.PipelineRunName, metav1.GetOptions{})
+	pr, err := runcnx.Clients.Tekton.TektonV1beta1().PipelineRuns(targetNS).Get(ctx, laststatus.PipelineRunName, metav1.GetOptions{})
 	assert.NilError(t, err)
 
 	assert.Equal(t, options.PullRequestEvent, pr.Labels["pipelinesascode.tekton.dev/event-type"])
