337a65835348adfb1f940949dc296657da5dd086
Ralph Bean <rbean@redhat.com>
2022-09-13 12:29:31 -0400

    Escape square brackets in k8s label values
    This problem arises whenever the `pipelinesascode.tekton.dev/sender`
    label is set for a pipelinerun trigger by a commit pushed by
    a GitHub bot. The bot's name `github-actions[bot]` is not a valid
    label value, which causes the PipelineRun to fail admission.
    
    This adds additional characters to the existing escape list,
    handling the case of github bot names.
    
    Related https://issues.redhat.com/browse/SRVKP-2461

--- pkg/formatting/k8labels.go
+++ pkg/formatting/k8labels.go
@@ -6,6 +6,6 @@ import "strings"
 // full ref, we replace the "/" by "-". The tools probably need to be aware of
 // it when querying.
 func K8LabelsCleanup(s string) string {
-	replasoeur := strings.NewReplacer("/", "-", " ", "_")
+	replasoeur := strings.NewReplacer("/", "-", " ", "_", "[", "__", "]", "__")
 	return replasoeur.Replace(s)
 }
