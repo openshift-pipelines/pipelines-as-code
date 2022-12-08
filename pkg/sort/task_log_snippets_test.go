31469c4a74cf7c0ad8dcda2e56de3f3edcd4f3af
Chmouel Boudjnah <chmouel@redhat.com>
2022-12-08 06:48:59 +0100

    Improve unittest coverage in sort package
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

---
+++ pkg/sort/task_log_snippets_test.go
@@ -0,0 +1,48 @@
+package sort
+
+import (
+	"testing"
+	"time"
+
+	pacv1alpha1 "github.com/openshift-pipelines/pipelines-as-code/pkg/apis/pipelinesascode/v1alpha1"
+	"gotest.tools/v3/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestTaskInfos(t *testing.T) {
+	type args struct {
+		taskinfos map[string]pacv1alpha1.TaskInfos
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "test sort",
+			args: args{
+				taskinfos: map[string]pacv1alpha1.TaskInfos{
+					"task-after": {
+						Name: "after",
+						CompletionTime: &metav1.Time{
+							Time: time.Date(2021, 1, 2, 0, 0, 0, 0, time.UTC),
+						},
+					},
+					"task-before": {
+						Name: "before",
+						CompletionTime: &metav1.Time{
+							Time: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+						},
+					},
+				},
+			},
+			want: "before",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := TaskInfos(tt.args.taskinfos)
+			assert.Equal(t, tt.want, got[0].Name)
+		})
+	}
+}
