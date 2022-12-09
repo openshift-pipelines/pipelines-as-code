bf339f708f771ac87aa5989901456d7c5442a0b4
Chmouel Boudjnah <chmouel@redhat.com>
2022-12-09 12:28:51 +0100

    Update doc regarding the temp installation token
    The token is available for 8h not 30s as i was previously wrong (the api
    come back with expired_token but that field is completely nonsense).
    
    Added a clear example on how to reuse it with github-add-comment hub
    task.
    
    Fixes #1011
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- pkg/secrets/basic_auth.go
+++ pkg/secrets/basic_auth.go
@@ -56,9 +56,8 @@ func MakeBasicAuthSecret(runevent *info.Event, secretName string) (*corev1.Secre
 	secretData := map[string]string{
 		".gitconfig":       fmt.Sprintf(basicAuthGitConfigData, cloneURL),
 		".git-credentials": urlWithToken,
-		// that token is going to be very short lived on github app due of the
-		// way it works with ghappinstallation library, we need another way to
-		// do this and expose a token with a longer ttl
+		// On GitHub APP the token is available for 8h if you have the user to server token expiration.
+		// the token is scopped to the installation ID
 		"git-provider-token": token,
 	}
 	annotations := map[string]string{
