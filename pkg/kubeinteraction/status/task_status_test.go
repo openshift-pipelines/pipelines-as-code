f1d07aaa3c64c6f9b9c173e48a15c820007f0c1f
Chmouel Boudjnah <chmouel@redhat.com>
2023-02-06 15:16:58 +0100

    Add unit test for GetStatusFromTaskStatusOrFromAsking
    and move it to kubeinteraction/status package instead of sort
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- pkg/kubeinteraction/status/task_status_test.go
+++ pkg/kubeinteraction/status/task_status_test.go
@@ -6,12 +6,17 @@ import (
 
 	"github.com/jonboulle/clockwork"
 	"github.com/openshift-pipelines/pipelines-as-code/pkg/params"
+	paramclients "github.com/openshift-pipelines/pipelines-as-code/pkg/params/clients"
+	testclient "github.com/openshift-pipelines/pipelines-as-code/pkg/test/clients"
 	"github.com/openshift-pipelines/pipelines-as-code/pkg/test/kubernetestint"
 	tektontest "github.com/openshift-pipelines/pipelines-as-code/pkg/test/tekton"
 	tektonv1beta1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+	"go.uber.org/zap"
+	zapobserver "go.uber.org/zap/zaptest/observer"
 	"gotest.tools/v3/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	knativeapi "knative.dev/pkg/apis"
 	knativeduckv1 "knative.dev/pkg/apis/duck/v1"
 	rtesting "knative.dev/pkg/reconciler/testing"
@@ -94,6 +99,129 @@ func TestCollectFailedTasksLogSnippet(t *testing.T) {
 			assert.Equal(t, tt.wantFailure, len(got))
 			if tt.podOutput != "" {
 				assert.Equal(t, tt.podOutput, got["task1"].LogSnippet)
+			}
+		})
+	}
+}
+
+func TestGetStatusFromTaskStatusOrFromAsking(t *testing.T) {
+	testNS := "test"
+	tests := []struct {
+		name               string
+		pr                 *tektonv1beta1.PipelineRun
+		numStatus          int
+		expectedLogSnippet string
+		taskRuns           []*tektonv1beta1.TaskRun
+	}{
+		{
+			name:      "get status from PR pre 0.44 tektoncd/pipelines",
+			numStatus: 2,
+			pr: &tektonv1beta1.PipelineRun{
+				Status: tektonv1beta1.PipelineRunStatus{
+					PipelineRunStatusFields: tektonv1beta1.PipelineRunStatusFields{
+						TaskRuns: map[string]*tektonv1beta1.PipelineRunTaskRunStatus{
+							"status1": nil,
+							"status2": nil,
+						},
+					},
+				},
+			},
+		},
+		{
+			name:      "get status from child references post tektoncd/pipelines 0.44",
+			numStatus: 2,
+			pr: &tektonv1beta1.PipelineRun{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: testNS,
+				},
+				Status: tektonv1beta1.PipelineRunStatus{
+					PipelineRunStatusFields: tektonv1beta1.PipelineRunStatusFields{
+						ChildReferences: []tektonv1beta1.ChildStatusReference{
+							{
+								TypeMeta: runtime.TypeMeta{
+									Kind: "TaskRun",
+								},
+								Name: "hello",
+							},
+							{
+								TypeMeta: runtime.TypeMeta{
+									Kind: "TaskRun",
+								},
+								Name: "yolo",
+							},
+						},
+					},
+				},
+			},
+			taskRuns: []*tektonv1beta1.TaskRun{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "hello",
+						Namespace: testNS,
+					},
+					Status: tektonv1beta1.TaskRunStatus{},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "yolo",
+						Namespace: testNS,
+					},
+					Status: tektonv1beta1.TaskRunStatus{},
+				},
+			},
+		},
+		{
+			name: "error get status from child references post tektoncd/pipelines 0.44",
+			pr: &tektonv1beta1.PipelineRun{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: testNS,
+				},
+				Status: tektonv1beta1.PipelineRunStatus{
+					PipelineRunStatusFields: tektonv1beta1.PipelineRunStatusFields{
+						ChildReferences: []tektonv1beta1.ChildStatusReference{
+							{
+								Name: "hello",
+							},
+							{
+								Name: "yolo",
+							},
+						},
+					},
+				},
+			},
+			expectedLogSnippet: "cannot get taskrun status",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			observer, obslog := zapobserver.New(zap.InfoLevel)
+			logger := zap.New(observer).Sugar()
+			ctx, _ := rtesting.SetupFakeContext(t)
+			run := params.New()
+
+			tdata := testclient.Data{
+				Namespaces: []*corev1.Namespace{
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: testNS,
+						},
+					},
+				},
+				TaskRuns: tt.taskRuns,
+			}
+			stdata, _ := testclient.SeedTestData(t, ctx, tdata)
+			run.Clients = paramclients.Clients{
+				Kube:   stdata.Kube,
+				Tekton: stdata.Pipeline,
+				Log:    logger,
+			}
+			statuses := GetStatusFromTaskStatusOrFromAsking(ctx, tt.pr, run)
+			assert.Equal(t, len(statuses), tt.numStatus)
+
+			if tt.expectedLogSnippet != "" {
+				logmsg := obslog.FilterMessageSnippet(tt.expectedLogSnippet).TakeAll()
+				assert.Assert(t, len(logmsg) > 0, "log messages", logmsg, tt.expectedLogSnippet)
 			}
 		})
 	}
