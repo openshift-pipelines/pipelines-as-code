48988bc3497a60d690fd3363d74ec265d6242e07
Chmouel Boudjnah <chmouel@redhat.com>
2023-03-21 12:55:31 +0100

    Update all dependencies
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- pkg/test/github/github.go
+++ pkg/test/github/github.go
@@ -13,7 +13,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/google/go-github/v49/github"
+	"github.com/google/go-github/v50/github"
 	"github.com/openshift-pipelines/pipelines-as-code/pkg/params/info"
 	"gotest.tools/v3/assert"
 )
