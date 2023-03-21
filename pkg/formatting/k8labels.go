0d2e115d12a186bd2f48f47f7621b3a6373383df
Chmouel Boudjnah <chmouel@redhat.com>
2023-03-21 10:25:14 +0100

    Make sure we clean the labels before creating the secrets.
    On Gitlab we have org that can be very long with slash included due of
    projects.
    
    So clean this up in the labels, or we won't be able to access it
    otherwise, ui can use the annotations
    
    Renamed the function to make it a bit more explicit
    
    Fixes #1199

--- pkg/formatting/k8labels.go
+++ pkg/formatting/k8labels.go
@@ -2,7 +2,7 @@ package formatting
 
 import "strings"
 
-// K8LabelsCleanup k8s do not like slash in labels value and on push we have the
+// CleanValueKubernetes k8s do not like slash in labels value and on push we have the
 // full ref, we replace the "/" by "-". The tools probably need to be aware of
 // it when querying.
 //
@@ -10,7 +10,7 @@ import "strings"
 // '-', '_' or '.', and must start and end with an alphanumeric character
 // (e.g. 'MyValue', or 'my_value', or '12345', regex used for validation is
 // '(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])?')
-func K8LabelsCleanup(s string) string {
+func CleanValueKubernetes(s string) string {
 	replasoeur := strings.NewReplacer("/", "-", " ", "_", "[", "__", "]", "__")
 	return replasoeur.Replace(strings.TrimRight(s, " -_[]"))
 }
