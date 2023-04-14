98297ce684566bad0d568ec9e6433292e7c9d6e2
Chmouel Boudjnah <chmouel@redhat.com>
2023-04-14 11:55:11 +0200

    include -f into the code part or it will look weird

--- pkg/params/run.go
+++ pkg/params/run.go
@@ -19,7 +19,7 @@ const (
 	StartingPipelineRunText = `Starting Pipelinerun <b>%s</b> in namespace
   <b>%s</b><br><br>You can follow the execution on the [%s](%s) PipelineRun viewer or via
   the command line with :
-	<br><code>%s pr logs -n %s %s</code> -f`
+	<br><code>%s pr logs -n %s %s -f</code>`
 	QueuingPipelineRunText = `PipelineRun <b>%s</b> has been queued Queuing in namespace
   <b>%s</b><br><br>`
 )
