cc09d062ebad528e04b201e6da81cf268a075dbe
Chmouel Boudjnah <chmouel@redhat.com>
2023-08-28 10:55:09 +0200

    update stabe tags only when releasing latest ver
    We were updating the stable tags every time even on minor versions, now
    make the tags only when we are sure it's the latest version.
    
    Add a small python script with the help of the packaging module to work
    this comparaison out.
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

---
+++ hack/compare-versions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Take a version as first argument (current)
+# Take a set of version delimited by commas as second argument
+# If the current is higher than all other versions return true otherwise false
+# example:
+# % python3 ./hack/compare-versions.py v1.1.0 v0.1.1,v0.2.2,v1.0.1
+# true
+# % python3 ./hack/compare-versions.py 0.1.0 0.1.1,0.2.2,1.0.1
+# false
+# semantic numbers with v is supported
+import sys
+
+from packaging import version
+
+if len(sys.argv[1]) == 0:
+    print("Usage compare-versions.py <current> <all versions separated by comma>")
+    sys.exit(1)
+
+current = sys.argv[1]
+all_versions = sys.argv[2].split(",")
+
+for i in all_versions:
+    if version.parse(current) < version.parse(i):
+        print("false")
+        sys.exit(1)
+
+print("true")
