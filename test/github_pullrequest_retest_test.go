9a63ecddc76f69783c9107838cd6389bbd969c99
savitaashture <sashture@redhat.com>
2023-11-14 16:21:08 +0530

    Add end-to-end testing for clean GitOps comment in the pull request
    Signed-off-by: Savita Ashture <sashture@gmail.com>

--- test/github_pullrequest_retest_test.go
+++ test/github_pullrequest_retest_test.go
@@ -22,7 +22,7 @@ func TestGithubPullRequestRetest(t *testing.T) {
 	if os.Getenv("NIGHTLY_E2E_TEST") != "true" {
 		t.Skip("Skipping test since only enabled for nightly")
 	}
-	ctx := context.TODO()
+	ctx := context.Background()
 	runcnx, ghcnx, opts, targetNS, targetRefName, prNumber, sha := tgithub.RunPullRequest(ctx, t,
 		"Github Retest comment", []string{"testdata/pipelinerun.yaml"}, false)
 	defer tgithub.TearDown(ctx, t, runcnx, ghcnx, prNumber, targetRefName, targetNS, opts)
@@ -48,41 +48,78 @@ func TestGithubPullRequestRetest(t *testing.T) {
 	runcnx.Clients.Log.Infof("Check if we have the repository set as succeeded")
 	repo, err := runcnx.Clients.PipelineAsCode.PipelinesascodeV1alpha1().Repositories(targetNS).Get(ctx, targetNS, metav1.GetOptions{})
 	assert.NilError(t, err)
-	assert.Assert(t, repo.Status[len(repo.Status)-1].Conditions[0].Status == corev1.ConditionTrue)
+	assert.Equal(t, repo.Status[len(repo.Status)-1].Conditions[0].Status, corev1.ConditionTrue)
 }
 
-func TestGithubPullRequestRetestForParticularPipelineRun(t *testing.T) {
-	ctx := context.TODO()
+func TestGithubPullRequestGitOpsComments(t *testing.T) {
+	ctx := context.Background()
 	runcnx, ghcnx, opts, targetNS, targetRefName, prNumber, sha := tgithub.RunPullRequest(ctx, t,
 		"Github Retest comment", []string{"testdata/pipelinerun.yaml", "testdata/pipelinerun-gitops.yaml"}, false)
 	defer tgithub.TearDown(ctx, t, runcnx, ghcnx, prNumber, targetRefName, targetNS, opts)
 
-	runcnx.Clients.Log.Infof("Creating /retest pipelinerun-name in PullRequest")
-	_, _, err := ghcnx.Client.Issues.CreateComment(ctx,
-		opts.Organization,
-		opts.Repo, prNumber,
-		&github.IssueComment{Body: github.String("/retest pr-gitops-comment")})
+	pruns, err := runcnx.Clients.Tekton.TektonV1().PipelineRuns(targetNS).List(ctx, metav1.ListOptions{})
 	assert.NilError(t, err)
+	assert.Equal(t, len(pruns.Items), 2)
 
-	runcnx.Clients.Log.Infof("Wait for the repository update to be updated")
-	waitOpts := twait.Opts{
-		RepoName:        targetNS,
-		Namespace:       targetNS,
-		MinNumberStatus: 3,
-		PollTimeout:     twait.DefaultTimeout,
-		TargetSHA:       sha,
+	tests := []struct {
+		name, comment string
+		prNum         int
+	}{
+		{
+			name:    "Retest",
+			comment: "/retest pr-gitops-comment",
+			prNum:   3,
+		},
+		{
+			name:    "Test and Cancel PipelineRun",
+			comment: "/cancel pr-gitops-comment",
+			prNum:   4,
+		},
 	}
-	err = twait.UntilRepositoryUpdated(ctx, runcnx.Clients, waitOpts)
-	assert.NilError(t, err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			waitOpts := twait.Opts{
+				RepoName:        targetNS,
+				Namespace:       targetNS,
+				MinNumberStatus: tt.prNum,
+				PollTimeout:     twait.DefaultTimeout,
+				TargetSHA:       sha,
+			}
+			if tt.comment == "/cancel pr-gitops-comment" {
+				runcnx.Clients.Log.Info("/test pr-gitops-comment on Pull Request before canceling")
+				_, _, err := ghcnx.Client.Issues.CreateComment(ctx,
+					opts.Organization,
+					opts.Repo, prNumber,
+					&github.IssueComment{Body: github.String("/test pr-gitops-comment")})
+				assert.NilError(t, err)
+				err = twait.UntilPipelineRunCreated(ctx, runcnx.Clients, waitOpts)
+				assert.NilError(t, err)
+			}
+			runcnx.Clients.Log.Infof("%s on Pull Request", tt.comment)
+			_, _, err = ghcnx.Client.Issues.CreateComment(ctx,
+				opts.Organization,
+				opts.Repo, prNumber,
+				&github.IssueComment{Body: github.String(tt.comment)})
+			assert.NilError(t, err)
 
-	runcnx.Clients.Log.Infof("Check if we have the repository set as succeeded")
-	repo, err := runcnx.Clients.PipelineAsCode.PipelinesascodeV1alpha1().Repositories(targetNS).Get(ctx, targetNS, metav1.GetOptions{})
-	assert.NilError(t, err)
-	assert.Assert(t, repo.Status[len(repo.Status)-1].Conditions[0].Status == corev1.ConditionTrue)
+			runcnx.Clients.Log.Info("Waiting for Repository to be updated")
+			err = twait.UntilRepositoryUpdated(ctx, runcnx.Clients, waitOpts)
+			assert.NilError(t, err)
 
-	prs, err := runcnx.Clients.Tekton.TektonV1().PipelineRuns(targetNS).List(ctx, metav1.ListOptions{
-		LabelSelector: fmt.Sprintf("%s=%s", keys.SHA, sha),
-	})
-	assert.NilError(t, err)
-	assert.Equal(t, len(prs.Items), 3)
+			runcnx.Clients.Log.Infof("Check if we have the repository set as succeeded")
+			repo, err := runcnx.Clients.PipelineAsCode.PipelinesascodeV1alpha1().Repositories(targetNS).Get(ctx, targetNS, metav1.GetOptions{})
+			assert.NilError(t, err)
+			if tt.comment == "/cancel pr-gitops-comment" {
+				assert.Equal(t, repo.Status[len(repo.Status)-1].Conditions[0].Status, corev1.ConditionFalse)
+			} else {
+				assert.Equal(t, repo.Status[len(repo.Status)-1].Conditions[0].Status, corev1.ConditionTrue)
+			}
+
+			pruns, err = runcnx.Clients.Tekton.TektonV1().PipelineRuns(targetNS).List(ctx, metav1.ListOptions{
+				LabelSelector: fmt.Sprintf("%s=%s", keys.SHA, sha),
+			})
+			assert.NilError(t, err)
+			assert.Equal(t, len(pruns.Items), tt.prNum)
+		})
+	}
 }
