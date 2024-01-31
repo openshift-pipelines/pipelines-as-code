87cd93a79f057b29a8b07cdb50d20d919307fcef
Chmouel Boudjnah <chmouel@redhat.com>
2024-01-31 15:40:30 +0100

    Make sure to make the secret conforms to k8 naming
    Extend the function CleanValueKubernetes to make sure the secret name is
    conform to dns subdomain name as per according to the k8 documentation
    
    Fixes #1570
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- pkg/formatting/k8labels.go
+++ pkg/formatting/k8labels.go
@@ -1,17 +1,25 @@
 package formatting
 
-import "strings"
+import (
+	"strings"
+)
 
-// CleanValueKubernetes k8s do not like slash in labels value and on push we have the
-// full ref, we replace the "/" by "-". The tools probably need to be aware of
-// it when querying.
-//
-// valid label must be an empty string or consist of alphanumeric characters,
-// '-', '_' or '.', and must start and end with an alphanumeric character
-// (e.g. 'MyValue', or 'my_value', or '12345', regex used for validation is
-// '(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])?').
+// CleanValueKubernetes conform a string to kubernetes naming convention
+// see https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#dns-subdomain-names
+// rules are:
+// • contain at most 63 characters
+// • contain only lowercase alphanumeric characters or '-'
+// • start with an alphanumeric character
+// • end with an alphanumeric character.
 func CleanValueKubernetes(s string) string {
-	replasoeur := strings.NewReplacer("/", "-", " ", "_", "[", "__", "]", "__")
-	replaced := replasoeur.Replace(strings.TrimRight(s, " -_[]"))
+	if len(s) >= 63 {
+		// keep the last 62 characters
+		s = s[len(s)-62:]
+	}
+
+	replasoeur := strings.NewReplacer(":", "-", "/", "-", " ", "_", "[", "__", "]", "__")
+	s = strings.TrimRight(s, " -_[]")
+	s = strings.TrimLeft(s, " -_[]")
+	replaced := replasoeur.Replace(s)
 	return strings.TrimSpace(replaced)
 }
