acba41d0d81fbf86bbac540715746976152ff7a2
Shivam Mukhade <shivam.mukhade@wooga.net>
2024-04-02 08:47:53 +0200

    Adds configutils package to validate and assign config
    finally we add a package to replace code in settings for validation
    and defaults with a generic func, using json tag for defaulting. and for
    figuring out which field it is from configmap.

---
+++ pkg/configutil/config.go
@@ -0,0 +1,98 @@
+package configutil
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+
+	"go.uber.org/zap"
+)
+
+func ValidateAndAssignValues(logger *zap.SugaredLogger, configData map[string]string, configStruct interface{}, customValidations map[string]func(string) error) error {
+	structValue := reflect.ValueOf(configStruct).Elem()
+	structType := reflect.TypeOf(configStruct).Elem()
+
+	var errors []error
+
+	for i := 0; i < structType.NumField(); i++ {
+		field := structType.Field(i)
+		fieldName := field.Name
+
+		jsonTag := field.Tag.Get("json")
+		// Skip field which doesn't have json tag
+		if jsonTag == "" {
+			continue
+		}
+
+		// Read value from ConfigMap
+		fieldValue := configData[strings.ToLower(jsonTag)]
+
+		// If value is missing in ConfigMap, use default value from struct tag
+		if fieldValue == "" {
+			fieldValue = field.Tag.Get("default")
+			if fieldValue == "" {
+				// Skip field if default value is not provided
+				continue
+			}
+		}
+
+		fieldValueKind := field.Type.Kind()
+
+		//nolint
+		switch fieldValueKind {
+		case reflect.String:
+			if validator, ok := customValidations[fieldName]; ok {
+				if err := validator(fieldValue); err != nil {
+					errors = append(errors, fmt.Errorf("custom validation failed for field %s: %w", fieldName, err))
+					continue
+				}
+			}
+			oldValue := structValue.FieldByName(fieldName).String()
+			if oldValue != fieldValue {
+				logger.Infof("updating value for field %s: from '%s' to '%s'", fieldName, oldValue, fieldValue)
+			}
+			structValue.FieldByName(fieldName).SetString(fieldValue)
+
+		case reflect.Bool:
+			boolValue, err := strconv.ParseBool(fieldValue)
+			if err != nil {
+				errors = append(errors, fmt.Errorf("invalid value for bool field %s: %w", fieldName, err))
+				continue
+			}
+			oldValue := structValue.FieldByName(fieldName).Bool()
+			if oldValue != boolValue {
+				logger.Infof("updating value for field %s: from '%v' to '%v'", fieldName, oldValue, boolValue)
+			}
+			structValue.FieldByName(fieldName).SetBool(boolValue)
+
+		case reflect.Int:
+			if validator, ok := customValidations[fieldName]; ok {
+				if err := validator(fieldValue); err != nil {
+					errors = append(errors, fmt.Errorf("custom validation failed for field %s: %w", fieldName, err))
+					continue
+				}
+			}
+			intValue, err := strconv.ParseInt(fieldValue, 10, 64)
+			if err != nil {
+				errors = append(errors, fmt.Errorf("invalid value for int field %s: %w", fieldName, err))
+				continue
+			}
+			oldValue := structValue.FieldByName(fieldName).Int()
+			if oldValue != intValue {
+				logger.Infof("updating value for field %s: from '%d' to '%d'", fieldName, oldValue, intValue)
+			}
+			structValue.FieldByName(fieldName).SetInt(intValue)
+
+		default:
+			// Skip unsupported field types
+			continue
+		}
+	}
+
+	if len(errors) > 0 {
+		return fmt.Errorf("validation errors: %v", errors)
+	}
+
+	return nil
+}
--- pkg/params/settings/config_test.go
+++ pkg/params/settings/config_test.go
@@ -1,127 +1,158 @@
 package settings
 
 import (
+	"reflect"
 	"testing"
 
-	"go.uber.org/zap"
-	zapobserver "go.uber.org/zap/zaptest/observer"
+	"github.com/openshift-pipelines/pipelines-as-code/pkg/test/logger"
+	"gotest.tools/v3/assert"
 )
 
-func TestStringToBool(t *testing.T) {
-	type args struct {
-		s string
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		{
-			name: "true",
-			args: args{
-				s: "true",
-			},
-			want: true,
-		},
-		{
-			name: "false",
-			args: args{
-				s: "false",
-			},
-			want: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := StringToBool(tt.args.s); got != tt.want {
-				t.Errorf("StringToBool() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestConfigToSettings(t *testing.T) {
-	type args struct {
-		setting *Settings
-		config  map[string]string
-	}
-	tests := []struct {
-		name            string
-		args            args
-		wantErr         bool
-		wantLogContains string
+	logger, _ := logger.GetLogger()
+
+	testCases := []struct {
+		name           string
+		configMap      map[string]string
+		expectedStruct Settings
+		expectedError  string
 	}{
 		{
-			name: "invalid",
-			args: args{
-				setting: &Settings{},
-				config: map[string]string{
-					SecretAutoCreateKey: "test",
-				},
-			},
-			wantErr: true,
-		},
-		{
-			name: "set application name",
-			args: args{
-				setting: &Settings{},
-				config: map[string]string{
-					ApplicationNameKey: "test",
-				},
-			},
-			wantLogContains: "application name set to test",
-		},
-		{
-			name: "set auto create key",
-			args: args{
-				setting: &Settings{},
-				config: map[string]string{
-					SecretAutoCreateKey: "true",
-				},
-			},
-			wantLogContains: "secret auto create",
-		},
-		{
-			name: "set remember-ok-to-test key",
-			args: args{
-				setting: &Settings{RememberOKToTest: true},
-				config: map[string]string{
-					RememberOKToTestKey: "false",
-				},
-			},
-			wantLogContains: "remember ok-to-test",
-		},
-		{
-			name: "set hub url",
-			args: args{
-				setting: &Settings{},
-				config: map[string]string{
-					HubURLKey: "https://test",
-				},
+			name:      "With all default values",
+			configMap: map[string]string{},
+			expectedStruct: Settings{
+				ApplicationName:                    "Pipelines as Code CI",
+				HubCatalogs:                        nil,
+				RemoteTasks:                        true,
+				MaxKeepRunsUpperLimit:              0,
+				DefaultMaxKeepRuns:                 0,
+				BitbucketCloudCheckSourceIP:        true,
+				BitbucketCloudAdditionalSourceIP:   "",
+				TektonDashboardURL:                 "",
+				AutoConfigureNewGitHubRepo:         false,
+				AutoConfigureRepoNamespaceTemplate: "",
+				SecretAutoCreation:                 true,
+				SecretGHAppRepoScoped:              true,
+				SecretGhAppTokenScopedExtraRepos:   "",
+				ErrorLogSnippet:                    true,
+				ErrorDetection:                     true,
+				ErrorDetectionNumberOfLines:        50,
+				ErrorDetectionSimpleRegexp:         "^(?P<filename>[^:]*):(?P<line>[0-9]+):(?P<column>[0-9]+):([ ]*)?(?P<error>.*)",
+				CustomConsoleName:                  "",
+				CustomConsoleURL:                   "",
+				CustomConsolePRdetail:              "",
+				CustomConsolePRTaskLog:             "",
+				CustomConsoleNamespaceURL:          "",
+				RememberOKToTest:                   true,
 			},
 		},
 		{
-			name: "set hub name",
-			args: args{
-				setting: &Settings{},
-				config: map[string]string{
-					HubCatalogNameKey: "foo",
-				},
+			name: "override values",
+			configMap: map[string]string{
+				"application-name":                       "pac-pac",
+				"remote-tasks":                           "false",
+				"max-keep-run-upper-limit":               "10",
+				"default-max-keep-runs":                  "5",
+				"bitbucket-cloud-check-source-ip":        "false",
+				"bitbucket-cloud-additional-source-ip":   "some-ip",
+				"tekton-dashboard-url":                   "https://tekton-dashboard",
+				"auto-configure-new-github-repo":         "true",
+				"auto-configure-repo-namespace-template": "template",
+				"secret-auto-create":                     "false",
+				"secret-github-app-token-scoped":         "false",
+				"secret-github-app-scope-extra-repos":    "extra-repos",
+				"error-log-snippet":                      "false",
+				"error-detection-from-container-logs":    "false",
+				"error-detection-max-number-of-lines":    "100",
+				"error-detection-simple-regexp":          "^(?P<filename>[^:]*):(?P<line>[0-9]+):(?P<column>[0-9]+):([ ]*)?(?P<error>.*)",
+				"custom-console-name":                    "custom-console",
+				"custom-console-url":                     "https://custom-console",
+				"custom-console-url-pr-details":          "https://custom-console-pr-details",
+				"custom-console-url-pr-tasklog":          "https://custom-console-pr-tasklog",
+				"custom-console-url-namespace":           "https://custom-console-namespace",
+				"remember-ok-to-test":                    "false",
 			},
+			expectedStruct: Settings{
+				ApplicationName:                    "pac-pac",
+				HubCatalogs:                        nil,
+				RemoteTasks:                        false,
+				MaxKeepRunsUpperLimit:              10,
+				DefaultMaxKeepRuns:                 5,
+				BitbucketCloudCheckSourceIP:        false,
+				BitbucketCloudAdditionalSourceIP:   "some-ip",
+				TektonDashboardURL:                 "https://tekton-dashboard",
+				AutoConfigureNewGitHubRepo:         true,
+				AutoConfigureRepoNamespaceTemplate: "template",
+				SecretAutoCreation:                 false,
+				SecretGHAppRepoScoped:              false,
+				SecretGhAppTokenScopedExtraRepos:   "extra-repos",
+				ErrorLogSnippet:                    false,
+				ErrorDetection:                     false,
+				ErrorDetectionNumberOfLines:        100,
+				ErrorDetectionSimpleRegexp:         "^(?P<filename>[^:]*):(?P<line>[0-9]+):(?P<column>[0-9]+):([ ]*)?(?P<error>.*)",
+				CustomConsoleName:                  "custom-console",
+				CustomConsoleURL:                   "https://custom-console",
+				CustomConsolePRdetail:              "https://custom-console-pr-details",
+				CustomConsolePRTaskLog:             "https://custom-console-pr-tasklog",
+				CustomConsoleNamespaceURL:          "https://custom-console-namespace",
+				RememberOKToTest:                   false,
+			},
+		},
+		{
+			name: "invalid value for bool field",
+			configMap: map[string]string{
+				"remote-tasks": "invalid",
+			},
+			expectedError: "invalid value for bool field RemoteTasks: strconv.ParseBool: parsing \"invalid\": invalid syntax",
+		},
+		{
+			name: "invalid value for int field",
+			configMap: map[string]string{
+				"max-keep-run-upper-limit": "invalid",
+			},
+			expectedError: "invalid value for int field MaxKeepRunsUpperLimit: strconv.ParseInt: parsing \"invalid\": invalid syntax",
+		},
+		{
+			name: "invalid value regex",
+			configMap: map[string]string{
+				"error-detection-simple-regexp": "[",
+			},
+			expectedError: "custom validation failed for field ErrorDetectionSimpleRegexp: invalid regex: error parsing regexp: missing closing ]: `[`",
+		},
+		{
+			name: "invalid value url",
+			configMap: map[string]string{
+				"tekton-dashboard-url": "invalid-url",
+			},
+			expectedError: "custom validation failed for field TektonDashboardURL: invalid value for URL, error: parse \"invalid-url\": invalid URI for request",
+		},
+		{
+			name: "invalid value url for custom console pr detail",
+			configMap: map[string]string{
+				"custom-console-url-pr-tasklog": "invalid-url",
+			},
+			expectedError: "custom validation failed for field CustomConsolePRTaskLog: invalid value, must start with http:// or https://",
 		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			observer, catchlog := zapobserver.New(zap.InfoLevel)
-			logger := zap.New(observer).Sugar()
-			if err := ConfigToSettings(logger, tt.args.setting, tt.args.config); (err != nil) != tt.wantErr {
-				t.Errorf("ConfigToSettings() error = %v, wantErr %v", err, tt.wantErr)
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var test Settings
+
+			err := ConfigToSettings(logger, &test, tc.configMap)
+
+			// set hub catalogs to nil to avoid comparison error
+			// test separately
+			test.HubCatalogs = nil
+
+			if tc.expectedError != "" {
+				assert.ErrorContains(t, err, tc.expectedError)
+				return
 			}
-			if tt.wantLogContains != "" {
-				all := catchlog.FilterMessageSnippet(tt.wantLogContains).TakeAll()
-				if len(all) == 0 {
-					t.Errorf("ConfigToSettings() want log contains %v given: %v", tt.wantLogContains, all)
-				}
+			assert.NilError(t, err)
+
+			if !reflect.DeepEqual(test, tc.expectedStruct) {
+				t.Errorf("failure, actual and expected struct:\nActual: %#v\nExpected: %#v", test, tc.expectedStruct)
 			}
 		})
 	}
