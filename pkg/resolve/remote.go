9ba9da2a0b48a1369af68b28851fbede45495e4c
Piyush Garg <pgarg@redhat.com>
2024-09-02 13:07:09 +0530

    Redesign PAC Resolver
    This will redesign resolver to work with
    multiple edge case scenarios.
    
    Previously pac resolver was filtering
    pipelinerun based on annotations but it was
    resolving all pipelines in .tekton dir
    leading to resolving unnecessary pipelines
    and other issue was it was storing task
    based on task name, instead of annotation name
    and version, so different version of task
    were not used across pipelineruns in .tekton dir
    
    Now with new design we are first filtering
    pipelinerun based on annotations, and then
    processing all pipelineruns one by one
    and only resolving pipeline related to these
    pipelineruns. Also we are now maintaining map
    of tasks with name and version at event level
    to not re fetch the task and now inline spec
    replacement in pipelinerun is done one by one
    so respective task as mentioned in annotation
    with name and version is used
    
    Also before filtering the pipelineruns, we should
    make sure that no two pipelineruns exists with
    same name in .tekton dir
    
    Added tests for the three scenarios

--- pkg/resolve/remote.go
+++ pkg/resolve/remote.go
@@ -5,22 +5,21 @@ import (
 	"fmt"
 
 	"github.com/openshift-pipelines/pipelines-as-code/pkg/matcher"
+	tektonv1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
 )
 
 type NamedItem interface {
 	GetName() string
 }
 
-func alreadySeen[T NamedItem](items []T, item T) bool {
-	for _, value := range items {
-		if value.GetName() == item.GetName() {
-			return true
-		}
+func alreadyFetchedResource[T NamedItem](resources map[string]T, resourceName string) bool {
+	if _, ok := resources[resourceName]; ok {
+		return true
 	}
 	return false
 }
 
-// getRemotes will get remote tasks or Pipelines from annotations.
+// resolveRemoteResources will get remote tasks or Pipelines from annotations.
 //
 // It already has some tasks or pipeline coming from the tekton directory stored in [types]
 //
@@ -32,74 +31,161 @@ func alreadySeen[T NamedItem](items []T, item T) bool {
 //
 // The precedence logic for Pipeline is first from PipelineRun annotations and
 // then from Tekton directory.
-func getRemotes(ctx context.Context, rt *matcher.RemoteTasks, types TektonTypes) (TektonTypes, error) {
-	remoteType := &TektonTypes{}
+func resolveRemoteResources(ctx context.Context, rt *matcher.RemoteTasks, types TektonTypes, ropt *Opts) ([]*tektonv1.PipelineRun, error) {
+	// contain Resources fetched for the event
+	fetchedResourcesForEvent := FetchedResources{
+		Tasks:     map[string]*tektonv1.Task{},
+		Pipelines: map[string]*tektonv1.Pipeline{},
+	}
+	pipelineRuns := []*tektonv1.PipelineRun{}
 	for _, pipelinerun := range types.PipelineRuns {
-		if len(pipelinerun.GetObjectMeta().GetAnnotations()) == 0 {
-			continue
+		// contain Resources specific to run
+		fetchedResourcesForPipelineRun := FetchedResourcesForRun{
+			Tasks: map[string]*tektonv1.Task{},
 		}
-
-		// get first all the tasks from the pipelinerun annotations
-		remoteTasks, err := rt.GetTaskFromAnnotations(ctx, pipelinerun.GetObjectMeta().GetAnnotations())
-		if err != nil {
-			return TektonTypes{}, fmt.Errorf("error getting remote task from pipelinerun annotations: %w", err)
-		}
-
-		for _, task := range remoteTasks {
-			if alreadySeen(remoteType.Tasks, task) {
-				rt.Logger.Debugf("skipping already fetched task %s in annotations on pipelinerun %s", task.GetName(), pipelinerun.GetName())
+		var pipeline *tektonv1.Pipeline
+		var err error
+		if ropt.RemoteTasks {
+			// no annotations on run, then skip
+			if pipelinerun.GetObjectMeta().GetAnnotations() == nil {
 				continue
 			}
-			remoteType.Tasks = append(remoteType.Tasks, task)
-		}
 
-		// get the pipeline from the remote annotation if any
-		remotePipeline, err := rt.GetPipelineFromAnnotations(ctx, pipelinerun.GetObjectMeta().GetAnnotations())
-		if err != nil {
-			return TektonTypes{}, fmt.Errorf("error getting remote pipeline from pipelinerun annotation: %w", err)
-		}
-
-		if remotePipeline != nil {
-			remoteType.Pipelines = append(remoteType.Pipelines, remotePipeline)
-		}
-	}
-
-	// grab the tasks from the remote pipeline
-	for _, pipeline := range remoteType.Pipelines {
-		if pipeline.GetObjectMeta().GetAnnotations() == nil {
-			continue
-		}
-		remoteTasks, err := rt.GetTaskFromAnnotations(ctx, pipeline.GetObjectMeta().GetAnnotations())
-		if err != nil {
-			return TektonTypes{}, fmt.Errorf("error getting remote tasks from remote pipeline %s: %w", pipeline.GetName(), err)
-		}
-
-		for _, remoteTask := range remoteTasks {
-			if alreadySeen(remoteType.Tasks, remoteTask) {
-				rt.Logger.Infof("skipping remote task %s from remote pipeline %s as already defined in pipelinerun", remoteTask.GetName(), pipeline.GetName())
+			if len(pipelinerun.GetObjectMeta().GetAnnotations()) == 0 {
 				continue
 			}
-			remoteType.Tasks = append(remoteType.Tasks, remoteTask)
-		}
-	}
 
-	ret := TektonTypes{
-		PipelineRuns: types.PipelineRuns,
-	}
-	// first get the remote types and then the local ones so remote takes precedence
-	for _, task := range append(remoteType.Tasks, types.Tasks...) {
-		if alreadySeen(ret.Tasks, task) {
-			rt.Logger.Infof("overriding task %s coming from tekton directory by an annotation task on the pipeline or pipelinerun", task.GetName())
-			continue
+			// get first all the pipeline from the pipelinerun annotations
+			remotePipeline, err := matcher.GrabPipelineFromAnnotations(pipelinerun.GetObjectMeta().GetAnnotations())
+			if err != nil {
+				return []*tektonv1.PipelineRun{}, fmt.Errorf("error getting remote pipeline from pipelinerun annotations: %w", err)
+			}
+
+			// if we got the pipeline name from annotation, we need to fetch the pipeline
+			if remotePipeline != "" {
+				// making sure that the pipeline with same annotation name is not fetched
+				if alreadyFetchedResource(fetchedResourcesForEvent.Pipelines, remotePipeline) {
+					rt.Logger.Debugf("skipping already fetched pipeline %s in annotations on pipelinerun %s", remotePipeline, pipelinerun.GetName())
+					// already fetched, then just get the pipeline to add to run specific Resources
+					pipeline = fetchedResourcesForEvent.Pipelines[remotePipeline]
+				} else {
+					// seems like a new pipeline, fetch it based on name in annotation
+					pipeline, err = rt.GetPipelineFromAnnotationName(ctx, remotePipeline)
+					if err != nil {
+						return []*tektonv1.PipelineRun{}, fmt.Errorf("error getting remote pipeline from pipelinerun annotations: %w", err)
+					}
+					// add the pipeline to the Resources fetched for the Event
+					fetchedResourcesForEvent.Pipelines[remotePipeline] = pipeline
+				}
+			}
 		}
-		ret.Tasks = append(ret.Tasks, task)
-	}
-	for _, remotePipeline := range append(remoteType.Pipelines, types.Pipelines...) {
-		if alreadySeen(ret.Pipelines, remotePipeline) {
-			rt.Logger.Infof("overriding pipeline %s coming from tekton directory by the annotation pipelinerun", remotePipeline.GetName())
-			continue
+		pipelineTasks := []string{}
+		// if run is referring to the pipelineRef and pipeline fetched from annotation have name equal to the pipelineRef
+		if pipelinerun.Spec.PipelineRef != nil && pipelinerun.Spec.PipelineRef.Resolver == "" {
+			if pipeline == nil || pipeline.Name != pipelinerun.Spec.PipelineRef.Name {
+				// if pipeline fetched from annotation is not having same name as PipelineRef, then we need to get a local pipeline if exist by same name
+				pipeline, err = getPipelineByName(pipelinerun.Spec.PipelineRef.Name, types.Pipelines)
+				if err != nil {
+					return []*tektonv1.PipelineRun{}, err
+				}
+			}
+			// add the pipeline to the run specific Resources
+			fetchedResourcesForPipelineRun.Pipeline = pipeline
+			// grab the tasks, that we may need to fetch for this pipeline from its annotations
+			if pipeline.GetObjectMeta().GetAnnotations() != nil {
+				// get all the tasks from the pipeline annotations
+				pipelineTasks, err = matcher.GrabTasksFromAnnotations(pipeline.GetObjectMeta().GetAnnotations())
+				if err != nil {
+					return []*tektonv1.PipelineRun{}, fmt.Errorf("error getting remote task from pipeline annotations: %w", err)
+				}
+			}
 		}
-		ret.Pipelines = append(ret.Pipelines, remotePipeline)
+
+		// now start fetching the tasks
+		if ropt.RemoteTasks {
+			// first get all the tasks from the pipelinerun annotations
+			remoteTasks, err := matcher.GrabTasksFromAnnotations(pipelinerun.GetObjectMeta().GetAnnotations())
+			if err != nil {
+				return []*tektonv1.PipelineRun{}, fmt.Errorf("error getting remote task from pipelinerun annotations: %w", err)
+			}
+
+			// now fetch all the tasks from pipelinerun and pipeline annotations, giving preference to pipelinerun annotation tasks
+			for _, remoteTask := range append(remoteTasks, pipelineTasks...) {
+				var task *tektonv1.Task
+				// if task is already fetched in the event, then just copy the task
+				if alreadyFetchedResource(fetchedResourcesForEvent.Tasks, remoteTask) {
+					rt.Logger.Debugf("skipping already fetched task %s in annotations on pipelinerun %s", remoteTask, pipelinerun.GetName())
+					task = fetchedResourcesForEvent.Tasks[remoteTask]
+				} else {
+					// get the task from annotation name
+					task, err = rt.GetTaskFromAnnotationName(ctx, remoteTask)
+					if err != nil {
+						return []*tektonv1.PipelineRun{}, fmt.Errorf("error getting remote task from pipelinerun annotations: %w", err)
+					}
+					// add the newly fetched tasks to fetchedResourcesForEvent with key annotation value
+					fetchedResourcesForEvent.Tasks[remoteTask] = task
+				}
+				// now checking if run specific resources already contain a task with same name, then don't add it
+				// this is to give preference to the pipelinerun annotation then pipeline annotation
+				if !alreadyFetchedResource(fetchedResourcesForPipelineRun.Tasks, task.GetName()) {
+					rt.Logger.Infof("skipping remote task %s as already fetched task %s for pipelinerun %s", remoteTask, task.GetName(), pipelinerun.GetName())
+					fetchedResourcesForPipelineRun.Tasks[task.GetName()] = task
+				}
+			}
+		}
+
+		// now add all the tasks in .tekton directory to Tasks, as we add them by default if not found in annotation
+		// we will skip the ones which exist in run specific resources with same name
+		for _, task := range types.Tasks {
+			if alreadyFetchedResource(fetchedResourcesForPipelineRun.Tasks, task.GetName()) {
+				rt.Logger.Infof("overriding task %s coming from .tekton directory by an annotation task for pipelinerun %s", task.GetName(), pipelinerun.GetName())
+				continue
+			}
+			fetchedResourcesForPipelineRun.Tasks[task.GetName()] = task
+		}
+
+		// if PipelineRef is used then, first resolve pipeline and replace all taskRef{Finally/Task} of Pipeline, then put inlinePipeline in PipelineRun
+		if pipelinerun.Spec.PipelineRef != nil && pipelinerun.Spec.PipelineRef.Resolver == "" {
+			pipelineResolved := fetchedResourcesForPipelineRun.Pipeline
+			turns, err := inlineTasks(pipelineResolved.Spec.Tasks, ropt, fetchedResourcesForPipelineRun)
+			if err != nil {
+				return nil, err
+			}
+			pipelineResolved.Spec.Tasks = turns
+
+			fruns, err := inlineTasks(pipelineResolved.Spec.Finally, ropt, fetchedResourcesForPipelineRun)
+			if err != nil {
+				return nil, err
+			}
+			pipelineResolved.Spec.Finally = fruns
+
+			pipelinerun.Spec.PipelineRef = nil
+			pipelinerun.Spec.PipelineSpec = &pipelineResolved.Spec
+		}
+
+		// if PipelineSpec is used then, now resolve the PipelineRun by replacing all taskRef{Finally/Task}
+		if pipelinerun.Spec.PipelineSpec != nil {
+			turns, err := inlineTasks(pipelinerun.Spec.PipelineSpec.Tasks, ropt, fetchedResourcesForPipelineRun)
+			if err != nil {
+				return nil, err
+			}
+			pipelinerun.Spec.PipelineSpec.Tasks = turns
+
+			fruns, err := inlineTasks(pipelinerun.Spec.PipelineSpec.Finally, ropt, fetchedResourcesForPipelineRun)
+			if err != nil {
+				return nil, err
+			}
+			pipelinerun.Spec.PipelineSpec.Finally = fruns
+		}
+
+		// Add a GenerateName based on the pipeline name and a "-"
+		// if we already have a GenerateName then just keep it like this
+		if ropt.GenerateName && pipelinerun.GenerateName == "" {
+			pipelinerun.GenerateName = pipelinerun.Name + "-"
+			pipelinerun.Name = ""
+		}
+		pipelineRuns = append(pipelineRuns, pipelinerun)
 	}
-	return ret, nil
+	// return all resolved PipelineRuns
+	return pipelineRuns, nil
 }
