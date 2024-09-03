7bb525fd2bb2a914fd4b1c34fbab8a6db0c2987d
Emil Natan <37400297+enarha@users.noreply.github.com>
2024-09-03 15:15:57 +0300

    Fix 503 http error when handling incoming webhook requests (#1747)
    Incoming webhook request especially involving GitHub Apps with many
    installations take minutes, so the existing http handler timeout of 10s
    was causing 503 failures. Once increased, OpenShift deployments were
    failing with http 504 (Gateway Time-out) because of the default 30s
    timeout on the pipelines-as-code-controller route.

--- pkg/adapter/adapter.go
+++ pkg/adapter/adapter.go
@@ -30,6 +30,11 @@ import (
 
 const globalAdapterPort = "8080"
 
+// For incoming webhook requests and GitHub Apps with many installations the handler takes long
+// e.g GitHub App with ~400 installations, it takes ~180s. For OpenShift deployments this also
+// requires matching timeout on the pipelines-as-code-controller route (default is 30s).
+const httpTimeoutHandler = 600 * time.Second
+
 type envConfig struct {
 	adapter.EnvConfig
 }
@@ -87,7 +92,7 @@ func (l *listener) Start(ctx context.Context) error {
 	srv := &http.Server{
 		Addr: ":" + adapterPort,
 		Handler: http.TimeoutHandler(mux,
-			10*time.Second, "Listener Timeout!\n"),
+			httpTimeoutHandler, "Listener Timeout!\n"),
 	}
 
 	enabled, tlsCertFile, tlsKeyFile := l.isTLSEnabled()
