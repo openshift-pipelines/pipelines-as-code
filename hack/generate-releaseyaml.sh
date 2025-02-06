1ce8970b57b20e1a0943f86aa81163c36e525d59
Chmouel Boudjnah <chmouel@redhat.com>
2025-02-06 14:03:03 +0100

    Fix image path in generate-releaseyaml.sh
    and generate some changes in go by rephrasing some of the sentences in
    cli webhook gitlab.
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- hack/generate-releaseyaml.sh
+++ hack/generate-releaseyaml.sh
@@ -2,8 +2,8 @@
 set -euf
 
 export TARGET_REPO_CONTROLLER=${TARGET_REPO_CONTROLLER:-ghcr.io/openshift-pipelines/pipelines-as-code/pipelines-as-code/pipelines-as-code/pipelines-as-code/pipelines-as-code-controller}
-export TARGET_REPO_WATCHER=${TARGET_REPO_WATCHER:-ghcr.io/openshift-pipelines/pipelines-as-code/pipelines-as-code/pipelines-as-code/pipelines-as-code/pipelines-as-code-watcher}
-export TARGET_REPO_WEBHOOK=${TARGET_REPO_WEBHOOK:-ghcr.io/openshift-pipelines/pipelines-as-code/pipelines-as-code/pipelines-as-code/pipelines-as-code/pipelines-as-code-webhook}
+export TARGET_REPO_WATCHER=${TARGET_REPO_WATCHER:-ghcr.io/openshift-pipelines/pipelines-as-code/pipelines-as-code-watcher}
+export TARGET_REPO_WEBHOOK=${TARGET_REPO_WEBHOOK:-ghcr.io/openshift-pipelines/pipelines-as-code/pipelines-as-code-webhook}
 export TARGET_BRANCH=${TARGET_BRANCH:-main}
 export TARGET_NAMESPACE=${TARGET_NAMESPACE:-pipelines-as-code}
 export TARGET_OPENSHIFT=${TARGET_OPENSHIFT:-""}
@@ -15,44 +15,43 @@ trap clean EXIT
 
 MODE=${1:-""}
 
-if [[ -n ${MODE} && ${MODE} == ko ]];then
-    tmpfile=$(mktemp /tmp/.mm.XXXXXX)
-    clean() { rm -f ${tmpfile}; }
-    trap clean EXIT
-    ko resolve -f config/ > ${tmpfile}
+if [[ -n ${MODE} && ${MODE} == ko ]]; then
+  tmpfile=$(mktemp /tmp/.mm.XXXXXX)
+  clean() { rm -f ${tmpfile}; }
+  trap clean EXIT
+  ko resolve -f config/ >${tmpfile}
 
-    if [[ ${TARGET_OPENSHIFT} != "" ]];then
-       ko resolve -f config/openshift >> ${tmpfile}
-    fi
+  if [[ ${TARGET_OPENSHIFT} != "" ]]; then
+    ko resolve -f config/openshift >>${tmpfile}
+  fi
 
-    files="${tmpfile}"
+  files="${tmpfile}"
 else
-    files=$(find config -maxdepth 1 -name '*.yaml'|sort -n)
+  files=$(find config -maxdepth 1 -name '*.yaml' | sort -n)
 
-    if [[ ${TARGET_OPENSHIFT} != "" ]];then
-       files="${files} $(find config/openshift -maxdepth 1 -name '*.yaml'|sort -n)"
-    fi
+  if [[ ${TARGET_OPENSHIFT} != "" ]]; then
+    files="${files} $(find config/openshift -maxdepth 1 -name '*.yaml' | sort -n)"
+  fi
 fi
 
+for file in ${files}; do
+  sed -e '/^$/d' -e '/^#/d' ${file} | head -1 | grep -q -- "---" || echo -e "---\n"
+  sed -r -e "s,(.*image:.*)ko://github.com/openshift-pipelines/pipelines-as-code/cmd/pipelines-as-code-controller.*,\1${TARGET_REPO_CONTROLLER}:${TARGET_BRANCH}\"," \
+    -r -e "s,(.*image:.*)ko://github.com/openshift-pipelines/pipelines-as-code/cmd/pipelines-as-code-watcher.*,\1${TARGET_REPO_WATCHER}:${TARGET_BRANCH}\"," \
+    -r -e "s,(.*image:.*)ko://github.com/openshift-pipelines/pipelines-as-code/cmd/pipelines-as-code-webhook.*,\1${TARGET_REPO_WEBHOOK}:${TARGET_BRANCH}\"," \
+    -e "s/(namespace: )\w+.*/\1${TARGET_NAMESPACE}/g" \
+    -e "s,app.kubernetes.io/version:.*,app.kubernetes.io/version: \"${TARGET_PAC_VERSION}\"," \
+    -e "s/Copyright[ ]*[0-9]{4}/Copyright $(date "+%Y")/" \
+    -e "/kind: Namespace$/ { n;n;s/name: .*/name: ${TARGET_NAMESPACE}/;}" \
+    -e "s/\"devel\"/\"${TARGET_PAC_VERSION}\"/" \
+    ${file} >${TMP}
 
-for file in ${files};do
-    sed -e '/^$/d' -e '/^#/d' ${file} | head -1 | grep -q -- "---" || echo -e "---\n"
-    sed -r -e "s,(.*image:.*)ko://github.com/openshift-pipelines/pipelines-as-code/cmd/pipelines-as-code-controller.*,\1${TARGET_REPO_CONTROLLER}:${TARGET_BRANCH}\"," \
-        -r -e "s,(.*image:.*)ko://github.com/openshift-pipelines/pipelines-as-code/cmd/pipelines-as-code-watcher.*,\1${TARGET_REPO_WATCHER}:${TARGET_BRANCH}\"," \
-        -r -e "s,(.*image:.*)ko://github.com/openshift-pipelines/pipelines-as-code/cmd/pipelines-as-code-webhook.*,\1${TARGET_REPO_WEBHOOK}:${TARGET_BRANCH}\"," \
-        -e "s/(namespace: )\w+.*/\1${TARGET_NAMESPACE}/g" \
-        -e "s,app.kubernetes.io/version:.*,app.kubernetes.io/version: \"${TARGET_PAC_VERSION}\"," \
-        -e "s/Copyright[ ]*[0-9]{4}/Copyright $(date "+%Y")/" \
-        -e "/kind: Namespace$/ { n;n;s/name: .*/name: ${TARGET_NAMESPACE}/;}" \
-        -e "s/\"devel\"/\"${TARGET_PAC_VERSION}\"/" \
-        ${file} > ${TMP}
+  # Remove openshift stuff apiGroups if we are not targetting openshift...
+  [[ -z ${TARGET_OPENSHIFT} ]] && {
+    sed -ir '/^[ ]*- apiGroups:.*route.openshift.io/,/verbs.*/d' ${TMP}
+  }
 
-    # Remove openshift stuff apiGroups if we are not targetting openshift...
-    [[ -z ${TARGET_OPENSHIFT} ]] && {
-        sed -ir '/^[ ]*- apiGroups:.*route.openshift.io/,/verbs.*/d' ${TMP}
-    }
-
-    echo "" >> ${TMP}
-    tail -1 ${TMP} |grep -q "^$" && sed -i '$d' ${TMP} >> /tmp/aaaa
-    cat ${TMP}
+  echo "" >>${TMP}
+  tail -1 ${TMP} | grep -q "^$" && sed -i '$d' ${TMP} >>/tmp/aaaa
+  cat ${TMP}
 done
