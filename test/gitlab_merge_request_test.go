c08fdf4a93ac7da5a1f901db1de7e6da783de3af
Andrew Thorp <athorp@redhat.com>
2025-03-19 11:38:59 -0400

    fix: Cancel PipelineRuns on MR change in Gitlab
    When a Merge Request in Gitlab triggers a PipelineRun, the TriggerTarget
    is updated to "pull_request" but the EventType label intentionally
    remains as "Merge Request", unchanged from the event. This appears to be
    done intentionally. As a result, when querying for PipelineRuns related
    to an event with a PullRequest Trigger type using the EventType label,
    we have to include 'Merge_Request' as a possible label value
    
    Note: 'Merge_Request' is used instead of the event's raw string
    'Merge Request' because <space> is an illegal character in labels and
    is sanitized in labels.AddLabelsAndAnnotations using
    formatting.CleanValueKubernetes

--- test/gitlab_merge_request_test.go
+++ test/gitlab_merge_request_test.go
@@ -20,6 +20,7 @@ import (
 	"github.com/openshift-pipelines/pipelines-as-code/test/pkg/payload"
 	"github.com/openshift-pipelines/pipelines-as-code/test/pkg/scm"
 	twait "github.com/openshift-pipelines/pipelines-as-code/test/pkg/wait"
+	v1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
 	"github.com/tektoncd/pipeline/pkg/names"
 	clientGitlab "gitlab.com/gitlab-org/api/client-go"
 	"gotest.tools/v3/assert"
@@ -275,6 +276,91 @@ func TestGitlabOnComment(t *testing.T) {
 
 	err = twait.RegexpMatchingInPodLog(context.Background(), runcnx, targetNS, fmt.Sprintf("tekton.dev/pipelineRun=%s", lastPrName), "step-task", *regexp.MustCompile(triggerComment), "", 2)
 	assert.NilError(t, err)
+}
+
+func TestGitlabCancelInProgressOnChange(t *testing.T) {
+	targetNS := names.SimpleNameGenerator.RestrictLengthWithRandomSuffix("pac-e2e-ns")
+	ctx := context.Background()
+	runcnx, opts, glprovider, err := tgitlab.Setup(ctx)
+	assert.NilError(t, err)
+	ctx, err = cctx.GetControllerCtxInfo(ctx, runcnx)
+	assert.NilError(t, err)
+	runcnx.Clients.Log.Info("Testing Gitlab cancel in progress on pr close")
+	projectinfo, resp, err := glprovider.Client.Projects.GetProject(opts.ProjectID, nil)
+	assert.NilError(t, err)
+	if resp != nil && resp.StatusCode == http.StatusNotFound {
+		t.Errorf("Repository %s not found in %s", opts.Organization, opts.Repo)
+	}
+
+	err = tgitlab.CreateCRD(ctx, projectinfo, runcnx, targetNS, nil)
+	assert.NilError(t, err)
+
+	entries, err := payload.GetEntries(map[string]string{
+		".tekton/in-progress.yaml": "testdata/pipelinerun-cancel-in-progress.yaml",
+	}, targetNS, projectinfo.DefaultBranch,
+		triggertype.PullRequest.String(), map[string]string{})
+	assert.NilError(t, err)
+	targetRefName := names.SimpleNameGenerator.RestrictLengthWithRandomSuffix("pac-e2e-test")
+
+	gitCloneURL, err := scm.MakeGitCloneURL(projectinfo.WebURL, opts.UserName, opts.Password)
+	assert.NilError(t, err)
+	mrTitle := "TestCancelInProgress initial commit - " + targetRefName
+	scmOpts := &scm.Opts{
+		GitURL:        gitCloneURL,
+		Log:           runcnx.Clients.Log,
+		WebURL:        projectinfo.WebURL,
+		TargetRefName: targetRefName,
+		BaseRefName:   projectinfo.DefaultBranch,
+		CommitTitle:   mrTitle,
+	}
+
+	oldSha := scm.PushFilesToRefGit(t, scmOpts, entries)
+	runcnx.Clients.Log.Infof("Branch %s has been created and pushed with files", targetRefName)
+	mrID, err := tgitlab.CreateMR(glprovider.Client, opts.ProjectID, targetRefName, projectinfo.DefaultBranch, mrTitle)
+	assert.NilError(t, err)
+	runcnx.Clients.Log.Infof("MergeRequest %s/-/merge_requests/%d has been created", projectinfo.WebURL, mrID)
+	defer tgitlab.TearDown(ctx, t, runcnx, glprovider, mrID, targetRefName, targetNS, opts.ProjectID)
+
+	runcnx.Clients.Log.Infof("Waiting for the pipelinerun to be created")
+	originalPipelineWaitOpts := twait.Opts{
+		RepoName:        targetNS,
+		Namespace:       targetNS,
+		MinNumberStatus: 1,
+		PollTimeout:     twait.DefaultTimeout,
+		TargetSHA:       oldSha,
+	}
+	err = twait.UntilPipelineRunCreated(ctx, runcnx.Clients, originalPipelineWaitOpts)
+	assert.NilError(t, err)
+
+	newEntries := map[string]string{
+		"new-file.txt": "plz work",
+	}
+
+	changeTitle := "TestCancelInProgress second commit - " + targetRefName
+	scmOpts = &scm.Opts{
+		GitURL:        gitCloneURL,
+		Log:           runcnx.Clients.Log,
+		WebURL:        projectinfo.WebURL,
+		TargetRefName: targetRefName,
+		BaseRefName:   targetRefName,
+		CommitTitle:   changeTitle,
+	}
+	newSha := scm.PushFilesToRefGit(t, scmOpts, newEntries)
+
+	runcnx.Clients.Log.Infof("Waiting for new pipeline to be created")
+	newPipelineWaitOpts := twait.Opts{
+		RepoName:        targetNS,
+		Namespace:       targetNS,
+		MinNumberStatus: 1,
+		PollTimeout:     twait.DefaultTimeout,
+		TargetSHA:       newSha,
+	}
+	err = twait.UntilPipelineRunCreated(ctx, runcnx.Clients, newPipelineWaitOpts)
+	assert.NilError(t, err)
+
+	runcnx.Clients.Log.Infof("Waiting for old pipelinerun to be canceled")
+	cancelledErr := twait.UntilPipelineRunHasReason(ctx, runcnx.Clients, v1.PipelineRunReasonCancelled, originalPipelineWaitOpts)
+	assert.NilError(t, cancelledErr)
 }
 
 func TestGitlabCancelInProgressOnPRClose(t *testing.T) {
