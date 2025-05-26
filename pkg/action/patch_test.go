a0bcb6f36cda49b85d60ab1b82b81dc330efd08f
Chmouel Boudjnah <cboudjna@redhat.com>
2025-05-26 20:20:47 +0200

    chore: Update controller finalizer name to include explicit part
    *   The finalizer name used by the controller was updated.
    *   A new constant `FinalizerName` was introduced for the explicit part of the
    name.
    *   The finalizer name was updated to combine the group name and the new
    constant.
    *   This change aligns the finalizer naming with common Kubernetes conventions.
    
    Fixes #1763
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>
    Co-authored-by: zakisk <zashaikh@redhat.com>

--- pkg/action/patch_test.go
+++ pkg/action/patch_test.go
@@ -1,7 +1,7 @@
 package action
 
 import (
-	"path/filepath"
+	"path"
 	"testing"
 
 	apipac "github.com/openshift-pipelines/pipelines-as-code/pkg/apis/pipelinesascode"
@@ -40,7 +40,7 @@ func TestPatchPipelineRun(t *testing.T) {
 
 	patchedPR, err := PatchPipelineRun(ctx, logger, "log URL", fakeClients.Tekton, testPR, getLogURLMergePatch(fakeClients, testPR))
 	assert.NilError(t, err)
-	assert.Equal(t, patchedPR.Annotations[filepath.Join(apipac.GroupName, "log-url")], "https://localhost.console/#/namespaces/namespace/pipelineruns/force-me")
+	assert.Equal(t, patchedPR.Annotations[path.Join(apipac.GroupName, "log-url")], "https://localhost.console/#/namespaces/namespace/pipelineruns/force-me")
 }
 
 func getLogURLMergePatch(clients clients.Clients, pr *pipelinev1.PipelineRun) map[string]any {
