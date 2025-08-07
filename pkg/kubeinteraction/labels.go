d9eb22ef65bde4440ac5c5be99823d4475729bf7
Gal Ben Haim <gbenhaim@redhat.com>
2025-08-07 09:55:28 +0300

    fix: Set state only after status report
    Set the state annotation and labels in pipelineascode only after
    the state was reported to the SCM.
    
    This is achieved by patching the PLR with the state label/annotation
    after it was created.
    
    This change is needed in order to avoid a case where the watcher will report
    a status before PAC.
    
    With this change the issue can't happen, since the watcher only reconciles
    PLRs that has the state annotation, so the first reconcile of the PLRs
    will happen only after PAC already sent the initial status to the SCM.
    
    In addition, simplify the condition which checks if the "queued" state
    annotation/label should be set. We can only check if the PLR is pending,
    it doesn't matter if the reason is because of PAC's concurrency control
    or an external controller.
    
    Signed-off-by: Gal Ben Haim <gbenhaim@redhat.com>

--- pkg/kubeinteraction/labels.go
+++ pkg/kubeinteraction/labels.go
@@ -55,7 +55,6 @@ func AddLabelsAndAnnotations(event *info.Event, pipelineRun *tektonv1.PipelineRu
 		keys.SourceBranch:  event.HeadBranch,
 		keys.Repository:    repo.GetName(),
 		keys.GitProvider:   providerConfig.Name,
-		keys.State:         StateStarted,
 		keys.ControllerInfo: fmt.Sprintf(`{"name":"%s","configmap":"%s","secret":"%s", "gRepo": "%s"}`,
 			paramsinfo.Controller.Name, paramsinfo.Controller.Configmap, paramsinfo.Controller.Secret, paramsinfo.Controller.GlobalRepository),
 	}
