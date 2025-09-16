95d7edaf73572ce9f2d1845d5620b2501c93a6c7
Zaki Shaikh <zashaikh@redhat.com>
2025-09-16 15:53:24 +0530

    Revert on-path-change on pr merge in bitbucket
    This reverts commit f9fd3f3f7ba68db845ec2a0b723798ed812f4a1e.
    
    Reason for Revert:
    The previous fix for SRVKP-7432 introduced an unexpected bug
    (SRVKP-8616) where the `{{ revision }}` dynamic variable in Pipelines as
    Code no longer fetches the correct, newest merge commit in Bitbucket
    Data Center. Instead, it was fetching the last commit of the source
    branch.
    
    Customer feedback indicates this change breaks their CI/CD workflow, as
    their teams rely on the merge-commit ID and do not wish to alter their
    git strategies. This is a critical blocker for them, preventing them
    from upgrading to versions 1.19.x and accessing other important fixes,
    such as the one for CAP-724.
    
    This revert restores the previous behavior, ensuring the `{{ revision
    }}` variable correctly references the merge commit. A long-term solution
    to address both issues (SRVKP-7432 and SRVKP-8616) will be developed in
    separate, newly-created tickets (SRVKP-8619 and SRVKP-8620).
    
    Fixes: SRVKP-8616
    Related: SRVKP-7432, SRVKP-8619, SRVKP-8620
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- test/pkg/bitbucketdatacenter/pr.go
+++ test/pkg/bitbucketdatacenter/pr.go
@@ -6,7 +6,9 @@ import (
 	"testing"
 
 	"github.com/openshift-pipelines/pipelines-as-code/pkg/params"
+	"github.com/openshift-pipelines/pipelines-as-code/pkg/params/triggertype"
 	"github.com/openshift-pipelines/pipelines-as-code/test/pkg/options"
+	"github.com/openshift-pipelines/pipelines-as-code/test/pkg/payload"
 	"github.com/openshift-pipelines/pipelines-as-code/test/pkg/scm"
 
 	goscm "github.com/jenkins-x/go-scm/scm"
@@ -14,24 +16,21 @@ import (
 )
 
 func CreatePR(ctx context.Context, t *testing.T, client *goscm.Client, runcnx *params.Run, opts options.E2E, repo *goscm.Repository, files map[string]string, orgAndRepo, targetNS string) *goscm.PullRequest {
-	baseBranchRef := repo.Branch
-	if opts.BaseBranch != "" {
-		baseBranchRef = opts.BaseBranch
-	}
-
-	branch, resp, err := client.Git.CreateRef(ctx, orgAndRepo, targetNS, baseBranchRef)
+	mainBranchRef := "refs/heads/main"
+	branch, resp, err := client.Git.CreateRef(ctx, orgAndRepo, targetNS, mainBranchRef)
 	assert.NilError(t, err, "error creating branch: http status code: %d : %v", resp.Status, err)
 	runcnx.Clients.Log.Infof("Branch %s has been created", branch.Name)
 
+	files, err = payload.GetEntries(files, targetNS, options.MainBranch, triggertype.PullRequest.String(), map[string]string{})
+	assert.NilError(t, err)
 	gitCloneURL, err := scm.MakeGitCloneURL(repo.Clone, opts.UserName, opts.Password)
 	assert.NilError(t, err)
-
 	scmOpts := &scm.Opts{
 		GitURL:        gitCloneURL,
 		Log:           runcnx.Clients.Log,
 		WebURL:        repo.Clone,
 		TargetRefName: targetNS,
-		BaseRefName:   baseBranchRef,
+		BaseRefName:   repo.Branch,
 		CommitTitle:   fmt.Sprintf("commit %s", targetNS),
 	}
 	scm.PushFilesToRefGit(t, scmOpts, files)
@@ -42,10 +41,9 @@ func CreatePR(ctx context.Context, t *testing.T, client *goscm.Client, runcnx *p
 		Title: title,
 		Body:  "Test PAC on bitbucket data center",
 		Head:  targetNS,
-		Base:  baseBranchRef,
+		Base:  "main",
 	}
 	pr, resp, err := client.PullRequests.Create(ctx, orgAndRepo, prOpts)
 	assert.NilError(t, err, "error creating pull request: http status code: %d : %v", resp.Status, err)
-	runcnx.Clients.Log.Infof("Created Pull Request with Title '%s'. Head branch '%s' ⮕ Base Branch '%s'", pr.Title, pr.Head.Ref, pr.Base.Ref)
 	return pr
 }
