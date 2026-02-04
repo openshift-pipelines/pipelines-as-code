8ec30e260880a29398c53939e02ccdbc83dfa6f9
Chmouel Boudjnah <chmouel@redhat.com>
2026-02-04 13:08:56 +0100

    fix: modify repo URL for log collection
    Modified the `NSTearDown` function to append a random suffix to
    repository URLs before deleting them. This ensures that any outstanding
    webhooks targeting these repositories do not trigger new runs during the
    teardown process, preventing potential interference or errors.
    
    The change was made because previously, repositories were deleted
    directly, which could lead to race conditions where webhooks were still
    active and attempted to create new pipeline runs in a namespace that was
    being cleaned up.
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- test/pkg/repository/teardown.go
+++ test/pkg/repository/teardown.go
@@ -6,14 +6,25 @@ import (
 	"testing"
 
 	"github.com/openshift-pipelines/pipelines-as-code/pkg/params"
+	"github.com/openshift-pipelines/pipelines-as-code/pkg/random"
 	"gotest.tools/v3/assert"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func NSTearDown(ctx context.Context, t *testing.T, runcnx *params.Run, targetNS string) {
-	runcnx.Clients.Log.Infof("Deleting Repository in %s", targetNS)
-	err := runcnx.Clients.PipelineAsCode.PipelinesascodeV1alpha1().Repositories(targetNS).DeleteCollection(ctx, metav1.DeleteOptions{}, metav1.ListOptions{})
+	repos, err := runcnx.Clients.PipelineAsCode.PipelinesascodeV1alpha1().Repositories(targetNS).List(ctx, metav1.ListOptions{})
 	assert.NilError(t, err)
+
+	// Add random suffix to the repo URL so we can keep it for logs collection (if PAC_E2E_KEEP_NS is set)
+	// so we can investigate later if needed and capture its status it captured
+	// we need to rename it so webhooks don't trigger any more runs
+	for _, repo := range repos.Items {
+		repo.Spec.URL += random.AlphaString(5)
+		if _, err := runcnx.Clients.PipelineAsCode.PipelinesascodeV1alpha1().Repositories(targetNS).Update(ctx, &repo, metav1.UpdateOptions{}); err != nil {
+			runcnx.Clients.Log.Warnf("Failed to renamae Repository URL %s: %v", repo.Name, err)
+		}
+	}
+
 	if os.Getenv("PAC_E2E_KEEP_NS") != "true" {
 		runcnx.Clients.Log.Infof("Deleting NS %s", targetNS)
 		err = runcnx.Clients.Kube.CoreV1().Namespaces().Delete(ctx, targetNS, metav1.DeleteOptions{})
