3c832c33913a6833c1e6253bd92286c2ec0e3e36
Chmouel Boudjnah <chmouel@redhat.com>
2026-02-23 13:55:15 +0100

    chore: re-enable tests for Bitbucket Data Center
    This reverts commit 982d3af2572bae766f08085ff085e15064d2ba08.

--- test/bitbucket_datacenter_push_test.go
+++ test/bitbucket_datacenter_push_test.go
@@ -19,7 +19,6 @@ import (
 )
 
 func TestBitbucketDataCenterCELPathChangeOnPush(t *testing.T) {
-	t.Skip("skipping test for now, until we ge the bitbucket infra back")
 	targetNS := names.SimpleNameGenerator.RestrictLengthWithRandomSuffix("pac-e2e-ns")
 	ctx := context.Background()
 	bitbucketWSOwner := os.Getenv("TEST_BITBUCKET_SERVER_E2E_REPOSITORY")
